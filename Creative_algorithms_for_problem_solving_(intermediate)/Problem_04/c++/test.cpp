45b03855a53b4def848f7f646dccc5b44e533648
heesoon <chipmaker0304@gmail.com>
2021-02-27 16:37:14 +0900

    implement c++ code

--- Creative_algorithms_for_problem_solving_(intermediate)/Problem_04/c++/test.cpp
+++ Creative_algorithms_for_problem_solving_(intermediate)/Problem_04/c++/test.cpp
@@ -1,87 +1,38 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
-const int N = 7;
-int depth = 0;
-int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, 1, 0, -1};
-int map[N][N] = {
-	{0, 1, 1, 0, 1, 0, 0},
-	{0, 1, 1, 0, 1, 0, 1},
-	{1, 1, 1, 0, 1, 0, 1},
-	{0, 0, 0, 0, 1, 1, 1},
-	{0, 1, 0, 0, 0, 0, 0},
-	{0, 1, 1, 1, 1, 1, 0},
-	{0, 1, 1, 1, 0, 0, 0}
-};
+int N = 0;
+int ans = 0;
+std::vector<int> col(10);
+std::vector<int> inc(20);
+std::vector<int> dec(20);
 
-std::vector<int> v;
-
-bool is_in_map(int y, int x)
+void solve(int y)
 {
-	if( (y >= 0 && y < N) && (x >= 0 && x < N) )
+	if(y > N)
 	{
-		return true;
+		ans++;
+		return;
 	}
-	
-	return false;
-}
 
-void dfs(int y, int x, int c)
-{
-	map[y][x] = c;
-    depth += 1;
-	
-	for(int i = 0; i < 4; i++)
+	for(int x = 1; x <= N; x++)
 	{
-		int ny = y + dy[i];
-		int nx = x + dx[i];
-
-		if( (is_in_map(ny, nx) == true) && (map[ny][nx] == 1) )
-		{		
-			dfs(ny, nx, c);
+		if(!col[x] && !inc[y+x] && !dec[N+(y-x)+1])
+		{
+			col[x] = inc[y+x] = dec[N+(y-x)+1] = 1;
+			solve(y+1);
+			col[x] = inc[y+x] = dec[N+(y-x)+1] = 0;
 		}
 	}
 }
 
 int main()
 {
-	int count = 1;
-	
-	for(int y = 0; y < N; y++)
-	{
-		for(int x = 0; x < N; x++)
-		{
-			if(map[y][x] == 1)
-			{
-				count++;
-				depth = 0;
-                dfs(y, x, count);
-				v.push_back(depth);
-			}
-		}
-	}
-	
-	std::cout << count -1 << std::endl;
-	std::sort(v.begin(), v.end(), [](int a, int b){
-		return a > b;
-	});
-	
-	std::vector<int>::iterator itor = v.begin();
-	for(; itor != v.end(); itor++)
-	{
-		std::cout << *itor << std::endl;
-	}
+	std::cout << "Enter N : ";
+	std::cin >> N;
+	std::cout << std::endl;
 
-	for(int y = 0; y < N; y++)
-	{
-		for(int x = 0; x < N; x++)
-		{
-            std::cout << map[y][x] << " ";
-		}
-        std::cout << std::endl;
-	}
- 
-    return 0;
+	solve(1);
+
+	std::cout << ans << std::endl;
 }

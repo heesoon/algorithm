b7e729f16b91d5df03b7790dec323667c5c586e1
김희순 <hskim@gimhuisun-ui-MacBookPro.local>
2021-03-03 22:50:08 +0900

    solve problem with c++

--- Creative_algorithms_for_problem_solving_(intermediate)/Problem_05/c++/test.cpp
+++ Creative_algorithms_for_problem_solving_(intermediate)/Problem_05/c++/test.cpp
@@ -1,22 +1,19 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
-const int N = 7;
-int depth = 0;
+const int N = 5;
+int depth = 999999;
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
-int map[N][N] = {
-	{0, 1, 1, 0, 1, 0, 0},
-	{0, 1, 1, 0, 1, 0, 1},
-	{1, 1, 1, 0, 1, 0, 1},
-	{0, 0, 0, 0, 1, 1, 1},
-	{0, 1, 0, 0, 0, 0, 0},
-	{0, 1, 1, 1, 1, 1, 0},
-	{0, 1, 1, 1, 0, 0, 0}
+char map[N][N] = {
+	{'#', 'S', '#', '#', '#'},
+	{'#', '.', '.', '.', '#'},
+	{'#', '.', '#', '.', '#'},
+	{'#', '.', '.', '.', '.'},
+	{'#', '#', '#', 'G', '#'}
 };
 
-std::vector<int> v;
+std::vector<std::vector<bool>> visit;
 
 bool is_in_map(int y, int x)
 {
@@ -28,60 +25,48 @@ bool is_in_map(int y, int x)
 	return false;
 }
 
-void dfs(int y, int x, int c)
+void find_short_path(int y, int x, int d)
 {
-	map[y][x] = c;
-    depth += 1;
-	
+	if(map[y][x] == 'G')
+	{
+		depth = depth > d ? d : depth;
+	}
+
 	for(int i = 0; i < 4; i++)
 	{
 		int ny = y + dy[i];
 		int nx = x + dx[i];
 
-		if( (is_in_map(ny, nx) == true) && (map[ny][nx] == 1) )
-		{		
-			dfs(ny, nx, c);
+		if( (map[ny][nx] == '.' || map[ny][nx] == 'G') && (visit[ny][nx] == false) && (is_in_map(ny, nx) == true) )
+		{
+			visit[ny][nx] = true;
+			find_short_path(ny, nx, d+1);
+			visit[ny][nx] = false;
 		}
 	}
 }
 
 int main()
 {
-	int count = 1;
-	
-	for(int y = 0; y < N; y++)
-	{
-		for(int x = 0; x < N; x++)
-		{
-			if(map[y][x] == 1)
-			{
-				count++;
-				depth = 0;
-                dfs(y, x, count);
-				v.push_back(depth);
-			}
-		}
-	}
-	
-	std::cout << count -1 << std::endl;
-	std::sort(v.begin(), v.end(), [](int a, int b){
-		return a > b;
-	});
-	
-	std::vector<int>::iterator itor = v.begin();
-	for(; itor != v.end(); itor++)
-	{
-		std::cout << *itor << std::endl;
-	}
+	visit.assign(N, std::vector<bool>(N, 0));
 
 	for(int y = 0; y < N; y++)
 	{
 		for(int x = 0; x < N; x++)
 		{
-            std::cout << map[y][x] << " ";
+			if(map[y][x] == 'S')
+			{
+				visit[y][x] = true;
+				find_short_path(y, x, 0);
+			}
 		}
-        std::cout << std::endl;
 	}
- 
+
+	if(depth == 999999)
+	{
+		depth = -1;
+	}
+
+	std::cout << depth << std::endl;
     return 0;
 }

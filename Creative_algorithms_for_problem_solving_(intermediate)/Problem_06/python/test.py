58190b3114ab3a29f32b1c4f08528d4e477c1cea
김희순 <hskim@gimhuisun-ui-MacBookPro.local>
2021-03-11 23:45:29 +0900

    implement python

--- Creative_algorithms_for_problem_solving_(intermediate)/Problem_06/python/test.py
+++ Creative_algorithms_for_problem_solving_(intermediate)/Problem_06/python/test.py
@@ -1,58 +1,26 @@
-import pprint
 
-N = 7
-depth = 0
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
-map = [
-    [0, 1, 1, 0, 1, 0, 0],
-    [0, 1, 1, 0, 1, 0, 1],
-    [1, 1, 1, 0, 1, 0, 1],
-    [0, 0, 0, 0, 1, 1, 1],
-    [0, 1, 0, 0, 0, 0, 0],
-    [0, 1, 1, 1, 1, 1, 0],
-    [0, 1, 1, 1, 0, 0, 0]
-]
-result = []
+N = 0
+cnt = 0
+chk = [[[0 for i in range(100)] for j in range(100)] for k in range(100)]
 
-def is_in_map(y, x):
-    if (x >= 0 and x < N) and (y >= 0 and y < N) :
-        return True
+def solve(a, b, c):
+    global chk
+    global cnt
+    if a + b + c == N:
+        if (a <= b) and (b <= c) and (a + b > c) and (chk[a][b][c] == 0):
+            chk[a][b][c] = 1
+            cnt = cnt + 1
+        return
     
-    return False
+    solve(a+1, b, c)
+    solve(a, b+1, c)
+    solve(a, b, c+1)
 
-def dfs(y, x, c):
-    global depth
-    depth += 1
-    map[y][x] = c
-
-    for i in range(0, 4):
-        ny = y + dy[i]
-        nx = x + dx[i]
-
-        if (is_in_map(ny, nx) == True) and (map[ny][nx] == 1) :
-            dfs(ny, nx, c)
-        
 def main():
-    count = 1
-
-    for y in range(0, N):
-        for x in range(0, N):
-            if map[y][x] == 1:
-                count += 1
-                global depth
-                depth = 0
-                dfs(y, x, count)
-                result.append(depth)
-
-    result.sort(reverse=True)
-    print(count-1)
-    for d in result:
-        print(d, end=" ")
-    print()
-    
-    print(*map, sep='\n')
-    #pprint.pprint(map,  width=100, indent=1)
+    global N
+    N = int(input('Enter Number : '))
+    solve(1, 1, 1)
+    print(cnt)
 
 if __name__ == "__main__":
     main()

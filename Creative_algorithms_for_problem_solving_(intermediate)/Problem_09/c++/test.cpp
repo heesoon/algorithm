5d938446ac09d2cf2e3ea2b6333343ce8893c339
김희순 <hskim@gimhuisun-ui-MacBookPro.local>
2021-03-15 21:26:39 +0900

    implemetate c++

--- Creative_algorithms_for_problem_solving_(intermediate)/Problem_09/c++/test.cpp
+++ Creative_algorithms_for_problem_solving_(intermediate)/Problem_09/c++/test.cpp
@@ -1,87 +1,148 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <tuple>
 
-const int N = 7;
-int depth = 0;
-int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, 1, 0, -1};
-int map[N][N] = {
-	{0, 1, 1, 0, 1, 0, 0},
-	{0, 1, 1, 0, 1, 0, 1},
-	{1, 1, 1, 0, 1, 0, 1},
-	{0, 0, 0, 0, 1, 1, 1},
-	{0, 1, 0, 0, 0, 0, 0},
-	{0, 1, 1, 1, 1, 1, 0},
-	{0, 1, 1, 1, 0, 0, 0}
+int net[20][20];
+std::vector<std::tuple<int, int, int>> data{{3, 2, 1}, {3, 3, 2}, {3, 6, 2}, {3, 7, 2}, {3, 8, 2}, {3, 9, 1},
+	{4, 3, 1}, {4, 4, 2}, {4, 9, 1},
+	{5, 4, 1}, {5, 5, 2},
+	{6, 5, 1}, {6, 6, 2}, {6, 7, 2},
+	{7, 3, 1}, {7, 4, 1}, {7, 6, 1},
+	{8, 7, 2}, {8, 8, 1},
 };
 
-std::vector<int> v;
-
-bool is_in_map(int y, int x)
+bool is_in_net(int y, int x)
 {
-	if( (y >= 0 && y < N) && (x >= 0 && x < N) )
-	{
+	if( (x >= 0 && x < 20) && (y >= 0 && y < 20) )
 		return true;
-	}
 	
 	return false;
 }
 
-void dfs(int y, int x, int c)
+bool check_right(int y, int x)
 {
-	map[y][x] = c;
-    depth += 1;
-	
-	for(int i = 0; i < 4; i++)
+	int cnt = 0;
+	int i = 0;
+	if( (is_in_net(y, x-1) == true) && (net[y][x] == net[y][x-1]) )
 	{
-		int ny = y + dy[i];
-		int nx = x + dx[i];
-
-		if( (is_in_map(ny, nx) == true) && (map[ny][nx] == 1) )
-		{		
-			dfs(ny, nx, c);
-		}
+		return false;
 	}
+
+	while( (x+i) < 20 )
+	{
+		if(net[y][x] == net[y][x+i])
+		{
+			cnt++;
+		}
+		i++;
+	}
+
+	if(cnt == 5)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+bool check_down_right(int y, int x)
+{
+	int cnt = 0;
+	int i = 0;
+	if( (is_in_net(y-1, x-1) == true) && (net[y][x] == net[y-1][x-1]) )
+	{
+		return false;
+	}
+
+	while( ((x+i) < 20) && ((y+i) < 20) )
+	{
+		if(net[y][x] == net[y+i][x+i])
+		{
+			cnt++;
+		}
+		i++;
+	}
+
+	if(cnt == 5)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+bool check_down(int y, int x)
+{
+	int cnt = 0;
+	int i = 0;
+	if( (is_in_net(y-1, x) == true) && (net[y][x] == net[y-1][x]) )
+	{
+		return false;
+	}
+
+	while( (y+i) < 20 )
+	{
+		if(net[y][x] == net[y+i][x])
+		{
+			cnt++;
+		}
+		i++;
+	}
+
+	if(cnt == 5)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+bool solve(int y, int x)
+{
+	if(check_right(y, x) == true)
+		return true;
+
+	if(check_down_right(y, x) == true)
+		return true;
+
+	if(check_down(y, x) == true)
+		return true;
+
+	return false;	
 }
 
 int main()
 {
-	int count = 1;
-	
-	for(int y = 0; y < N; y++)
+	for(auto v : data)
 	{
-		for(int x = 0; x < N; x++)
+		int x, y, d;
+		std::tie(y, x, d) = v;
+		net[y][x] = d; 
+	}
+
+/*
+	for(int y = 0; y < 20; y++)
+	{
+		for(int x = 0; x < 20; x++)
 		{
-			if(map[y][x] == 1)
+			std::cout << net[y][x] << " ";
+		}
+		std::cout << std::endl;
+	}
+*/
+	for(int y = 0; y < 20; y++)
+	{
+		for(int x = 0; x < 20; x++)
+		{
+			if(net[y][x] != 0)
 			{
-				count++;
-				depth = 0;
-                dfs(y, x, count);
-				v.push_back(depth);
+				if(solve(y, x) == true)
+				{
+					std::cout << y << " " << x << std::endl;
+				}
 			}
 		}
 	}
-	
-	std::cout << count -1 << std::endl;
-	std::sort(v.begin(), v.end(), [](int a, int b){
-		return a > b;
-	});
-	
-	std::vector<int>::iterator itor = v.begin();
-	for(; itor != v.end(); itor++)
-	{
-		std::cout << *itor << std::endl;
-	}
 
-	for(int y = 0; y < N; y++)
-	{
-		for(int x = 0; x < N; x++)
-		{
-            std::cout << map[y][x] << " ";
-		}
-        std::cout << std::endl;
-	}
- 
     return 0;
 }

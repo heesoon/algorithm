7360e302ea07cc330370d1e79665b58a7ea44aab
김희순/책임연구원/ID DevOps Project(heesoon.kim@lge.com) <heesoon.kim@lge.com>
2021-03-15 11:56:37 +0900

    implement python code

--- Creative_algorithms_for_problem_solving_(intermediate)/Problem_07/python/test.py
+++ Creative_algorithms_for_problem_solving_(intermediate)/Problem_07/python/test.py
@@ -1,58 +1,25 @@
-import pprint
 
-N = 7
-depth = 0
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
-map = [
-    [0, 1, 1, 0, 1, 0, 0],
-    [0, 1, 1, 0, 1, 0, 1],
-    [1, 1, 1, 0, 1, 0, 1],
-    [0, 0, 0, 0, 1, 1, 1],
-    [0, 1, 0, 0, 0, 0, 0],
-    [0, 1, 1, 1, 1, 1, 0],
-    [0, 1, 1, 1, 0, 0, 0]
-]
-result = []
-
-def is_in_map(y, x):
-    if (x >= 0 and x < N) and (y >= 0 and y < N) :
-        return True
-    
-    return False
-
-def dfs(y, x, c):
-    global depth
-    depth += 1
-    map[y][x] = c
-
-    for i in range(0, 4):
-        ny = y + dy[i]
-        nx = x + dx[i]
-
-        if (is_in_map(ny, nx) == True) and (map[ny][nx] == 1) :
-            dfs(ny, nx, c)
+def solve(v, n, w):
+    r = 0
+    for i in range(n - w):
+        ir = 0
+        for j in range(i, i + w):
+            ir += v[j]
         
-def main():
-    count = 1
-
-    for y in range(0, N):
-        for x in range(0, N):
-            if map[y][x] == 1:
-                count += 1
-                global depth
-                depth = 0
-                dfs(y, x, count)
-                result.append(depth)
-
-    result.sort(reverse=True)
-    print(count-1)
-    for d in result:
-        print(d, end=" ")
-    print()
+        if(ir > r):
+            r = ir
     
-    print(*map, sep='\n')
-    #pprint.pprint(map,  width=100, indent=1)
+    return r
+
+def main():
+    v = [1, 0, 2, 0, 4, 3]
+    n = 6
+    w = 3
+    r = 0
+    
+    r = solve(v, n, w)
+    
+    print(r)
 
 if __name__ == "__main__":
     main()

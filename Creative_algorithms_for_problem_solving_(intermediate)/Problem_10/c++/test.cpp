623c39042bd9e4add67181119a64fc552553f842
heesoon <chipmaker0304@gmail.com>
2021-03-18 13:43:25 +0900

    c++ initial upload

--- Creative_algorithms_for_problem_solving_(intermediate)/Problem_10/c++/test.cpp
+++ Creative_algorithms_for_problem_solving_(intermediate)/Problem_10/c++/test.cpp
@@ -1,87 +1,66 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <tuple>
 
-const int N = 7;
-int depth = 0;
-int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, 1, 0, -1};
-int map[N][N] = {
-	{0, 1, 1, 0, 1, 0, 0},
-	{0, 1, 1, 0, 1, 0, 1},
-	{1, 1, 1, 0, 1, 0, 1},
-	{0, 0, 0, 0, 1, 1, 1},
-	{0, 1, 0, 0, 0, 0, 0},
-	{0, 1, 1, 1, 1, 1, 0},
-	{0, 1, 1, 1, 0, 0, 0}
+const int N = 11;
+std::vector<std::tuple<int, int, int>> data ={
+	{1, 2, 47},
+	{1, 3, 69},
+	{2, 4, 57},
+	{2, 5, 124},
+	{3, 4, 37},
+	{3, 5, 59},
+	{3, 6, 86},
+	{4, 6, 27},
+	{4, 7, 94},
+	{5, 7, 21},
+	{6, 7, 40},
 };
 
-std::vector<int> v;
+int map[N][N];
+int bVisited[N][N];
 
-bool is_in_map(int y, int x)
+void debug()
 {
-	if( (y >= 0 && y < N) && (x >= 0 && x < N) )
+	for(int a = 0; a < N; a++)
 	{
-		return true;
-	}
-	
-	return false;
-}
-
-void dfs(int y, int x, int c)
-{
-	map[y][x] = c;
-    depth += 1;
-	
-	for(int i = 0; i < 4; i++)
-	{
-		int ny = y + dy[i];
-		int nx = x + dx[i];
-
-		if( (is_in_map(ny, nx) == true) && (map[ny][nx] == 1) )
-		{		
-			dfs(ny, nx, c);
+		for(int b = 0; b < N; b++)
+		{
+			std::cout.width(4);
+			//std::cout.fill('*');
+			std::cout << map[a][b];
 		}
-	}
+		std::cout << std::endl;
+	}	
 }
 
 int main()
 {
-	int count = 1;
-	
-	for(int y = 0; y < N; y++)
-	{
-		for(int x = 0; x < N; x++)
-		{
-			if(map[y][x] == 1)
-			{
-				count++;
-				depth = 0;
-                dfs(y, x, count);
-				v.push_back(depth);
-			}
-		}
-	}
-	
-	std::cout << count -1 << std::endl;
-	std::sort(v.begin(), v.end(), [](int a, int b){
-		return a > b;
-	});
-	
-	std::vector<int>::iterator itor = v.begin();
-	for(; itor != v.end(); itor++)
-	{
-		std::cout << *itor << std::endl;
-	}
+	int n = 8;
+/*	
+	std::cout << "Enter n : ";
+	std::cin >> n;
+	std::cout << std::endl;
+*/	
+	int m = 11;
+/*
+	std::cout << "Enter m : ";
+	std::cin >> m;
+	std::cout << std::endl;	
+*/
 
-	for(int y = 0; y < N; y++)
+	// update map by data
+	//for(std::vector<std::tuple<int, int, int> >::size_type i = 0; i < data.size(); i++)
+	for(int i = 0; i < m; i++)
 	{
-		for(int x = 0; x < N; x++)
-		{
-            std::cout << map[y][x] << " ";
-		}
-        std::cout << std::endl;
+		int a, b, d;
+		auto& t = data[i];
+		std::tie(a, b, d) = t;
+
+		map[a][b] = d;
 	}
- 
+	
+	//debug();
+	
     return 0;
 }

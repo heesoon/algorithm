c8998eafb6567a882071f341cc640d1249dc3a2c
heesoon <chipmaker0304@gmail.com>
2021-03-21 02:24:30 +0900

    implement c++ code

--- Creative_algorithms_for_problem_solving_(intermediate)/Problem_08/c++/test.cpp
+++ Creative_algorithms_for_problem_solving_(intermediate)/Problem_08/c++/test.cpp
@@ -1,87 +1,44 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
-const int N = 7;
-int depth = 0;
-int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, 1, 0, -1};
-int map[N][N] = {
-	{0, 1, 1, 0, 1, 0, 0},
-	{0, 1, 1, 0, 1, 0, 1},
-	{1, 1, 1, 0, 1, 0, 1},
-	{0, 0, 0, 0, 1, 1, 1},
-	{0, 1, 0, 0, 0, 0, 0},
-	{0, 1, 1, 1, 1, 1, 0},
-	{0, 1, 1, 1, 0, 0, 0}
+int N = 2, M = 6;
+int W = 1, H = 3;
+int net[100][100] = {
+	{1, 0, 2, 0, 4, 3},
+	{3, 4, 0, 2, 0, 1},
 };
 
-std::vector<int> v;
-
-bool is_in_map(int y, int x)
+int solve()
 {
-	if( (y >= 0 && y < N) && (x >= 0 && x < N) )
-	{
-		return true;
-	}
-	
-	return false;
-}
+	int ans = 0;
 
-void dfs(int y, int x, int c)
-{
-	map[y][x] = c;
-    depth += 1;
-	
-	for(int i = 0; i < 4; i++)
+	for(int y = 0; y < N-W+1; y++)
 	{
-		int ny = y + dy[i];
-		int nx = x + dx[i];
+		for(int x = 0; x < M-H+1; x++)
+		{
+			int r = 0;
+			for(int yy = y; yy < y+W; yy++)
+			{
+				for(int xx = x; xx < x+H; xx++)
+				{
+					r += net[yy][xx];
+				}
+			}
 
-		if( (is_in_map(ny, nx) == true) && (map[ny][nx] == 1) )
-		{		
-			dfs(ny, nx, c);
+			if(r > ans)
+			{
+				ans = r;
+			}
 		}
 	}
+
+	return ans;
 }
 
 int main()
 {
-	int count = 1;
-	
-	for(int y = 0; y < N; y++)
-	{
-		for(int x = 0; x < N; x++)
-		{
-			if(map[y][x] == 1)
-			{
-				count++;
-				depth = 0;
-                dfs(y, x, count);
-				v.push_back(depth);
-			}
-		}
-	}
-	
-	std::cout << count -1 << std::endl;
-	std::sort(v.begin(), v.end(), [](int a, int b){
-		return a > b;
-	});
-	
-	std::vector<int>::iterator itor = v.begin();
-	for(; itor != v.end(); itor++)
-	{
-		std::cout << *itor << std::endl;
-	}
+	int r = solve();
 
-	for(int y = 0; y < N; y++)
-	{
-		for(int x = 0; x < N; x++)
-		{
-            std::cout << map[y][x] << " ";
-		}
-        std::cout << std::endl;
-	}
- 
+	std::cout << r << std::endl;
     return 0;
 }

9f35a0d162dc63b7e250a1fd37ee6dc41b083fef
heesoon <chipmaker0304@gmail.com>
2021-03-21 17:26:05 +0900

    python implementation

--- Creative_algorithms_for_problem_solving_(intermediate)/Problem_08/python/test.py
+++ Creative_algorithms_for_problem_solving_(intermediate)/Problem_08/python/test.py
@@ -1,58 +1,32 @@
-import pprint
+N = 2
+M = 6
+W = 1
+H = 3
 
-N = 7
-depth = 0
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
-map = [
-    [0, 1, 1, 0, 1, 0, 0],
-    [0, 1, 1, 0, 1, 0, 1],
-    [1, 1, 1, 0, 1, 0, 1],
-    [0, 0, 0, 0, 1, 1, 1],
-    [0, 1, 0, 0, 0, 0, 0],
-    [0, 1, 1, 1, 1, 1, 0],
-    [0, 1, 1, 1, 0, 0, 0]
+net = [
+    [1, 0, 2, 0, 4, 3],
+    [3, 4, 0, 2, 0, 1]
 ]
-result = []
 
-def is_in_map(y, x):
-    if (x >= 0 and x < N) and (y >= 0 and y < N) :
-        return True
+def solve():
+    ans = 0
+
+    for y in range(0, N-W+1):
+        for x in range(0, M-H+1):
+            r = 0
+            for yy in range(y, y+W):
+                for xx in range(x, x+H):
+                    r += net[yy][xx]
+            
+            if r > ans:
+                ans = r
     
-    return False
+    return ans
 
-def dfs(y, x, c):
-    global depth
-    depth += 1
-    map[y][x] = c
-
-    for i in range(0, 4):
-        ny = y + dy[i]
-        nx = x + dx[i]
-
-        if (is_in_map(ny, nx) == True) and (map[ny][nx] == 1) :
-            dfs(ny, nx, c)
-        
 def main():
-    count = 1
+    r = solve()
 
-    for y in range(0, N):
-        for x in range(0, N):
-            if map[y][x] == 1:
-                count += 1
-                global depth
-                depth = 0
-                dfs(y, x, count)
-                result.append(depth)
-
-    result.sort(reverse=True)
-    print(count-1)
-    for d in result:
-        print(d, end=" ")
-    print()
-    
-    print(*map, sep='\n')
-    #pprint.pprint(map,  width=100, indent=1)
+    print(r)
 
 if __name__ == "__main__":
     main()

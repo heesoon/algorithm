bb8e0ab23027497ff719a3d0e6e8cad1b9ad17fe
heesoon <chipmaker0304@gmail.com>
2021-03-25 10:10:27 +0900

    implement c++ code

--- Creative_algorithms_for_problem_solving_(intermediate)/Problem_13/c++/test.cpp
+++ Creative_algorithms_for_problem_solving_(intermediate)/Problem_13/c++/test.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
+#include <vector>
+
+const int n = 3;
+int ans = 0x7FFFFFFF;
+int vx[n];
+
+std::vector<std::vector<int>> d = {
+	{1, 5, 3},
+	{2, 4, 7},
+	{5, 3, 5}
+};
+
+void solve(int y, int s)
+{
+	if(y == n)
+	{
+		if(ans > s)
+		{
+			ans = s;
+		}
+		
+		return;
+	}
+	
+	for(int i = 0; i < n; i++)
+	{
+		if(!vx[i])
+		{
+			vx[i] = 1;
+			solve(y+1, s+d[i][y]);
+			vx[i] = 0;
+		}
+	}
+}
 
 int main()
-{	
+{
+	solve(0, 0);
+	std::cout << ans << std::endl;
     return 0;
 }

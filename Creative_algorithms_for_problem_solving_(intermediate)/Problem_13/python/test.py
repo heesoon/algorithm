b899a67d93a047355ac8454eb22cd2328d29ec68
heesoon <chipmaker0304@gmail.com>
2021-03-25 10:35:30 +0900

    implement python

--- Creative_algorithms_for_problem_solving_(intermediate)/Problem_13/python/test.py
+++ Creative_algorithms_for_problem_solving_(intermediate)/Problem_13/python/test.py
@@ -1,58 +1,30 @@
-N = 11
-ans = 0x7fffffff
-
-Paths = [
-    [1, 2, 47],
-    [1, 3, 69],
-    [2, 4, 57],
-    [2, 5, 124],
-    [3, 4, 37],
-    [3, 5, 59],
-    [3, 6, 86],
-    [4, 6, 27],
-    [4, 7, 94],
-    [5, 7, 21],
-    [6, 7, 40]
+n = 3
+ans = 0x7FFFFFFF
+vx = [0 for i in range(n)]
+d = [
+	[1, 5, 3],
+	[2, 4, 7],
+	[5, 3, 5]
 ]
 
-V = [0 for i in range(N)]
-G = [[0 for i in range(N)] for j in range(N)]
-
-def debug():
-    for a in range(N):
-        for b in range(N):
-            print(G[a][b], end=" ")
-        print()
-
-def initialize():
-    for data in Paths:
-        (a, b, d) = data
-        G[a][b] = d
-        G[b][a] = d
-
-def solve(a, d, n):
+def solve(y, s):
+    global n
     global ans
-    if a == n:
-        if ans > d:
-            ans = d
+    global vx
+
+    if y == n:
+        if ans > s:
+            ans = s
         return
     
-    for b in range(1, n+1):
-        if V[b] == 0 and G[a][b] != 0:
-            V[b] = 1
-            solve(b, d+G[a][b], n)
-            V[b] = 0
+    for x in range(0, n):
+        if vx[x] == 0:
+            vx[x] = 1
+            solve(y+1, s+d[x][y])
+            vx[x] = 0
 
 def main():
-    n = 8
-    # n = input("input n : ")
-    m = 11
-    # m = input("input m : ")
-
-    initialize()
-
-    solve(1, 0, n-1)
-
+    solve(0, 0)
     print(ans)
 
 if __name__ == "__main__":

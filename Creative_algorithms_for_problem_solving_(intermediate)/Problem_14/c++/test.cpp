e7e1f28cc050532e25dbeec92ecdc73ccccb0145
heesoon <chipmaker0304@gmail.com>
2021-03-25 11:47:50 +0900

    c++ code implement

--- Creative_algorithms_for_problem_solving_(intermediate)/Problem_14/c++/test.cpp
+++ Creative_algorithms_for_problem_solving_(intermediate)/Problem_14/c++/test.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
 
+int ans = 0x7FFFFFFF;
+const int n = 5;
+const int m = 60;
+const int a[n] = {30, 10, 20, 35, 40};
+const int c[n] = {3, 0, 3, 5, 4};
+
+void solve(int i, int sa, int sc)
+{
+	if(i == n)
+	{
+		if(sa >= 60)
+		{
+			if(ans > sc)
+			{
+				ans = sc;
+			}
+		}
+		
+		return;
+	}
+	
+	// 현재 앱의 메모리를 해제하는 경우
+	solve(i+1, sa+a[i], sc+c[i]);
+	// 현재 앱의 메모리를 해제하지 않는 경우
+	solve(i+1, sa, sc);
+	
+	return;
+}
+
 int main()
-{	
+{
+	solve(0, 0, 0);
+	std::cout << ans << std::endl;
     return 0;
 }

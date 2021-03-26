f3c95f8466ccd65cfcce517afae54742c74130c2
heesoon <chipmaker0304@gmail.com>
2021-03-26 21:55:22 +0900

    add new version

--- Creative_algorithms_for_problem_solving_(intermediate)/Problem_16/c++/test.cpp
+++ Creative_algorithms_for_problem_solving_(intermediate)/Problem_16/c++/test.cpp
@@ -2,28 +2,8 @@
 #include <vector>
 #include <tuple>
 
-const int N = 12;
-const int TC = 2;
-std::vector<int> vn = {3, 9};
 std::vector<std::vector<int>> G;
 std::vector<int> V;
-std::vector<std::tuple<int,int>> data[TC] = {
-    {
-        {0, 1},
-        {1, 2},
-        {2, 0}
-    },
-    {
-        {0, 1},
-        {0, 2},
-        {0, 3},
-        {0, 4},
-        {0, 5},
-        {0, 6},
-        {0, 7},
-        {0, 8}                                                
-    },
-};
 
 void debug(const std::vector<std::vector<int>>& v)
 {
@@ -71,43 +51,97 @@ void solve(int v, int c, int n)
     }
 }
 
+void testCase1()
+{
+    int N = 3;
+    bool isPossible = true;
+
+    std::vector<std::tuple<int,int>> data = {
+        {0, 1},
+        {1, 2},
+        {2, 0}
+    };
+
+    G.assign(N, std::vector<int>(N, 0));
+    V.assign(N, 0);
+
+    std::vector<std::tuple<int,int>>::iterator it = data.begin();
+    for(; it != data.end(); it++)
+    {
+        int a = std::get<0>(*it);
+        int b = std::get<1>(*it);
+
+        G[a][b] = G[b][a] = 1;
+    }
+
+    solve(0, 1, N);
+
+    for(int i = 0; i < N; i++)
+    {
+        if(V[i] == 0)
+        {
+            isPossible = false;
+            break;
+        }
+    }
+
+    if(isPossible == false) std::cout << "IMPOSSIBLE" << std::endl;
+    else std::cout << "OK" << std::endl;
+
+    G.clear();
+    V.clear();
+}
+
+void testCase2()
+{
+    int N = 9;
+    bool isPossible = true;
+
+    std::vector<std::tuple<int,int>> data = {
+        {0, 1},
+        {0, 2},
+        {0, 3},
+        {0, 4},
+        {0, 5},
+        {0, 6},
+        {0, 7},
+        {0, 8}
+    };
+
+    G.assign(N, std::vector<int>(N, 0));
+    V.assign(N, 0);
+
+    std::vector<std::tuple<int,int>>::iterator it = data.begin();
+    for(; it != data.end(); it++)
+    {
+        int a = std::get<0>(*it);
+        int b = std::get<1>(*it);
+
+        G[a][b] = G[b][a] = 1;
+    }
+
+    solve(0, 1, N);
+
+    for(int i = 0; i < N; i++)
+    {
+        if(V[i] == 0)
+        {
+            isPossible = false;
+            break;
+        }
+    }
+
+    if(isPossible == false) std::cout << "IMPOSSIBLE" << std::endl;
+    else std::cout << "OK" << std::endl;
+
+    G.clear();
+    V.clear();
+}
+
 int main()
 {
-    for(int i = 0 ; i < TC; i++)
-    {
-        G.assign(N, std::vector<int>(N, 0));
-        V.assign(N, 0);
-
-        std::vector<std::tuple<int,int>>::iterator it = data[i].begin();
-        for(; it != data[i].end(); it++)
-        {
-            int a = std::get<0>(*it);
-            int b = std::get<1>(*it);
-
-            G[a][b] = G[b][a] = 1;
-        }
-
-        //debug(G);
-
-        solve(0, 1, vn[i]);
-
-        bool isPossible = true;
-        for(int i = 0; i < vn[i]; i++)
-        {
-            if(V[i] == 0)
-            {
-                isPossible = false;
-                break;
-            }
-        }
-
-        if(isPossible == false) std::cout << "IMPOSSIBLE" << std::endl;
-        else std::cout << "OK" << std::endl;
-
-        G.clear();
-        V.clear();
-
-    }
+    testCase1();
+    testCase2();
 
     return 0;
 }

1fc60c87fe16f16d478c962019ca35c73cee2c92
heesoon <chipmaker0304@gmail.com>
2021-03-26 14:40:07 +0900

    intermediate result

--- Creative_algorithms_for_problem_solving_(intermediate)/Problem_16/c++/test.cpp
+++ Creative_algorithms_for_problem_solving_(intermediate)/Problem_16/c++/test.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
+#include <vector>
+#include <tuple>
+
+int G[200][200];
+int visited[200];
+const int TC = 2;
+std::vector<std::tuple<int,int>> data[TC] = {
+    {
+        {0, 1},
+        {1, 2},
+        {2, 0}
+    },
+    {
+        {0, 1},
+        {0, 2},
+        {0, 3},
+        {0, 4},
+        {0, 5},
+        {0, 6},
+        {0, 7},
+        {0, 8}                                                
+    },
+};
 
 int main()
-{	
+{
+    for(int i = 0 ; i < TC; i++)
+    {       
+        std::vector<std::tuple<int,int>>::iterator it = data[i].begin();
+        for(; it != data[i].end(); it++)
+        {
+            int a = std::get<0>(*it);
+            int b = std::get<1>(*it);
+
+        }
+
+    }
+
     return 0;
 }

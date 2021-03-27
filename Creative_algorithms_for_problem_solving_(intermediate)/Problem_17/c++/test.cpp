bb3755fee554713d3131f984f6fdcb989ba3cc25
heesoon <chipmaker0304@gmail.com>
2021-03-28 00:43:10 +0900

    implement c++

--- Creative_algorithms_for_problem_solving_(intermediate)/Problem_17/c++/test.cpp
+++ Creative_algorithms_for_problem_solving_(intermediate)/Problem_17/c++/test.cpp
@@ -1,116 +1,41 @@
 #include <iostream>
 #include <vector>
-#include <tuple>
 
-const int N = 11;
-int value[N][N];
-int bVisited[N][N];
-int result = 0x7fffffff;
-
-std::vector<std::tuple<int, int, int>> data ={
-	{1, 2, 47},
-	{1, 3, 69},
-	{2, 4, 57},
-	{2, 5, 124},
-	{3, 4, 37},
-	{3, 5, 59},
-	{3, 6, 86},
-	{4, 6, 27},
-	{4, 7, 94},
-	{5, 7, 21},
-	{6, 7, 40},
-};
-
-std::vector<int> map[N];
-
-void debug()
+int solve1(const std::vector<int>& ar)
 {
-	for(int a = 0; a < N; a++)
+	int ans = 0xFFFFFFFF;
+
+	for(int a = 0; a < ar.size(); a++)
 	{
-		for(int b = 0; b < N; b++)
+		for(int b = a; b < ar.size(); b++)
 		{
-			std::cout.width(4);
-			//std::cout.fill('*');
-			std::cout << value[a][b];
+			int s = 0;
+			//std::cout << "[" << a << " ~ " << b << "] ";
+			for(int c = a; c <= b; c++)
+			{
+				s += ar[c];
+			}
+
+			//std::cout << s << std::endl;
+
+			if(s > ans)
+			{
+				ans = s;
+			}
 		}
-		std::cout << std::endl;
 	}
-	
-    std::cout << std::endl;
-	for(int a = 0; a < N; a++)
-	{
-        std::cout << a << " :";
-		for(int b = 0; b < map[a].size(); b++)
-		{
-			std::cout.width(2);
-			//std::cout.fill('*');
-			std::cout << map[a][b];
-		}
-		std::cout << std::endl;
-	}
+
+	return ans;
 }
 
-void solve(int a, int d, int n)
+void testCase1()
 {
-	if(a == n)
-	{
-		if(result > d)
-		{
-			result = d;
-		}
-		
-		return;
-	}
-
-	for(int i = 0; i < map[a].size(); i++)
-	{
-		int ni = map[a][i];
-
-		if(bVisited[a][ni] == 0 && bVisited[ni][a] == 0)
-		{
-			bVisited[a][ni] = 1;
-            bVisited[ni][a] = 1;
-			solve(map[a][i], d + value[a][ni], n);
-			bVisited[a][ni] = 0;
-            bVisited[ni][a] = 0;
-		}
-	}
+	std::vector<int> ar = {6, -7, 3, -1, 5, 2};
+	std::cout << solve1(ar) << std::endl;
 }
 
 int main()
 {
-	int n = 8;
-/*	
-	std::cout << "Enter n : ";
-	std::cin >> n;
-	std::cout << std::endl;
-*/	
-	int m = 11;
-/*
-	std::cout << "Enter m : ";
-	std::cin >> m;
-	std::cout << std::endl;	
-*/
-
-	// update map by data
-	//for(std::vector<std::tuple<int, int, int> >::size_type i = 0; i < data.size(); i++)
-	for(int i = 0; i < m; i++)
-	{
-		int a, b, d;
-		auto& t = data[i];
-		std::tie(a, b, d) = t;
-
-		value[a][b] = d;
-		map[a].push_back(b);
-		value[b][a] = d;
-		map[b].push_back(a);		
-	}
-	
-	//debug();
-	
-	solve(1, 0, n-1);
-	
-	std::cout << result << std::endl;
-	
+	testCase1();
     return 0;
 }

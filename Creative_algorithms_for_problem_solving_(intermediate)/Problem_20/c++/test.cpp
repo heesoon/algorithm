f26661f678fbfbac68bb43c67812051c337939bd
김희순 <hskim@gimhuisun-ui-MacBookPro.local>
2021-03-29 00:53:09 +0900

    modify code

--- Creative_algorithms_for_problem_solving_(intermediate)/Problem_20/c++/test.cpp
+++ Creative_algorithms_for_problem_solving_(intermediate)/Problem_20/c++/test.cpp
@@ -3,26 +3,26 @@
 
 int ans = 0;
 
-void solve(int m, std::vector<int>::size_type idx, const std::vector<int>& ar)
+void solve(std::vector<int>::size_type idx, int sum, int target, const std::vector<int>& ar)
 {
 	if(idx == ar.size())
 	{
-		if( m <= 40 && m > ans)
+		if( sum <= target && sum > ans)
 		{
-			ans = m;
+			ans = sum;
 		}
 
 		return;
 	}
 
-	solve(m+ar[idx], idx+1, ar);
-	solve(m, idx+1, ar);
+	solve(idx+1, sum+ar[idx], target, ar);
+	solve(idx+1, sum, target, ar);
 }
 
 void testCase1()
 {
 	std::vector<int> budgets = {7, 13, 17, 19, 29, 31};
-	solve(0, 0, budgets);
+	solve(0, 0, 40, budgets);
 
 	std::cout << ans << std::endl;
 	ans = 0;

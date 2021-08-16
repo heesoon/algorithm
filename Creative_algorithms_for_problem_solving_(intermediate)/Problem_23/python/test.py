ba2c210bd6ec8a35fa2b28ef81f6072028844c36
unknown <chipmaker0304@gmail.com>
2021-08-16 19:09:18 +0900

    implement python

--- Creative_algorithms_for_problem_solving_(intermediate)/Problem_23/python/test.py
+++ Creative_algorithms_for_problem_solving_(intermediate)/Problem_23/python/test.py
@@ -1,59 +1,23 @@
-N = 11
-ans = 0x7fffffff
 
-Paths = [
-    [1, 2, 47],
-    [1, 3, 69],
-    [2, 4, 57],
-    [2, 5, 124],
-    [3, 4, 37],
-    [3, 5, 59],
-    [3, 6, 86],
-    [4, 6, 27],
-    [4, 7, 94],
-    [5, 7, 21],
-    [6, 7, 40]
-]
+H = 3
+W = 4
+cnt = 0
 
-V = [0 for i in range(N)]
-G = [[0 for i in range(N)] for j in range(N)]
-
-def debug():
-    for a in range(N):
-        for b in range(N):
-            print(G[a][b], end=" ")
-        print()
-
-def initialize():
-    for data in Paths:
-        (a, b, d) = data
-        G[a][b] = d
-        G[b][a] = d
-
-def solve(a, d, n):
-    global ans
-    if a == n:
-        if ans > d:
-            ans = d
+def solve(h, w):
+    global H, W, cnt
+    if h > H or w > W+1:
         return
     
-    for b in range(1, n+1):
-        if V[b] == 0 and G[a][b] != 0:
-            V[b] = 1
-            solve(b, d+G[a][b], n)
-            V[b] = 0
+    if h == H and w == W+1:
+        cnt = cnt + 1
 
+    solve(h+1, w)
+    if float(H)/W <= float(h)/w :
+        solve(h, w+1)
+    
 def main():
-    n = 8
-    # n = input("input n : ")
-    m = 11
-    # m = input("input m : ")
+    solve(0, 1)
+    print(cnt)
 
-    initialize()
-
-    solve(1, 0, n-1)
-
-    print(ans)
-
-if __name__ == "__main__":
+if __name__ == "__main__" :
     main()

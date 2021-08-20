e3c4fba4c438af62fa083ef2a3cf624162ee644f
heesoon <chipmaker0304@gmail.com>
2021-08-20 17:26:50 +0900

    c++ implement

--- Creative_algorithms_for_problem_solving_(intermediate)/Problem_27/c++/test.cpp
+++ Creative_algorithms_for_problem_solving_(intermediate)/Problem_27/c++/test.cpp
@@ -1,116 +1,69 @@
 #include <iostream>
-#include <vector>
-#include <tuple>
 
-const int N = 11;
-int value[N][N];
-int bVisited[N][N];
-int result = 0x7fffffff;
+const int N = 8;
+int seq[80];
 
-std::vector<std::tuple<int, int, int>> data ={
-	{1, 2, 47},
-	{1, 3, 69},
-	{2, 4, 57},
-	{2, 5, 124},
-	{3, 4, 37},
-	{3, 5, 59},
-	{3, 6, 86},
-	{4, 6, 27},
-	{4, 7, 94},
-	{5, 7, 21},
-	{6, 7, 40},
-};
-
-std::vector<int> map[N];
-
-void debug()
-{
-	for(int a = 0; a < N; a++)
-	{
-		for(int b = 0; b < N; b++)
-		{
-			std::cout.width(4);
-			//std::cout.fill('*');
-			std::cout << value[a][b];
-		}
-		std::cout << std::endl;
-	}
-	
-    std::cout << std::endl;
-	for(int a = 0; a < N; a++)
-	{
-        std::cout << a << " :";
-		for(int b = 0; b < map[a].size(); b++)
-		{
-			std::cout.width(2);
-			//std::cout.fill('*');
-			std::cout << map[a][b];
-		}
-		std::cout << std::endl;
-	}
-}
-
-void solve(int a, int d, int n)
-{
-	if(a == n)
-	{
-		if(result > d)
-		{
-			result = d;
-		}
-		
+void solve(int n){
+	if(n == N){
+		dd
 		return;
 	}
 
-	for(int i = 0; i < map[a].size(); i++)
-	{
-		int ni = map[a][i];
-
-		if(bVisited[a][ni] == 0 && bVisited[ni][a] == 0)
-		{
-			bVisited[a][ni] = 1;
-            bVisited[ni][a] = 1;
-			solve(map[a][i], d + value[a][ni], n);
-			bVisited[a][ni] = 0;
-            bVisited[ni][a] = 0;
-		}
+	for(int i = 1; i <= 3; i++){
+		solve(n+1);
 	}
 }
 
-int main()
-{
-	int n = 8;
-/*	
-	std::cout << "Enter n : ";
-	std::cin >> n;
-	std::cout << std::endl;
-*/	
-	int m = 11;
-/*
-	std::cout << "Enter m : ";
-	std::cin >> m;
-	std::cout << std::endl;	
-*/
-
-	// update map by data
-	//for(std::vector<std::tuple<int, int, int> >::size_type i = 0; i < data.size(); i++)
-	for(int i = 0; i < m; i++)
-	{
-		int a, b, d;
-		auto& t = data[i];
-		std::tie(a, b, d) = t;
-
-		value[a][b] = d;
-		map[a].push_back(b);
-		value[b][a] = d;
-		map[b].push_back(a);		
-	}
-	
-	//debug();
-	
-	solve(1, 0, n-1);
-	
-	std::cout << result << std::endl;
-	
-    return 0;
+int main(){
+	return 0;
 }
+
+
+
+
+#if 0
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+#include <string>
+
+int x[4] = {2, 1, 2, 3};
+
+bool gs(int s, int e){
+    int m = (e - s)/2;
+    
+    if(e - s == 1){
+        if(x[e] == x[s])
+            return false;
+        else
+            return true;
+    }
+    else{
+        bool fgood = false;
+        for(int i = 0; i <= m; i++){
+            std::cout << "cmp : " << x[s+i] << ", " << x[m+1 +i] << std::endl;
+            if(x[s+i] != x[m+1 +i]){
+                std::cout << "good : " << x[s+i] << ", " << x[m+1 +i] << std::endl;
+                fgood = true;
+                break;
+            }
+        }
+        
+        if(fgood == false){
+            std::cout << "return false ... " << std::endl;
+            return false;
+        }
+        else{
+            std::cout << "divide ... " << std::endl;
+            return gs(s, m) && gs(m+1, e);
+        }
+    }
+}
+
+int main(){
+
+    std::cout << gs(0, 3) << std::endl;
+	return 0;
+}
+#endif

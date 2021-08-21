01c0be19a35616abc98783cb4fd6978ff5fc5e26
heesoon <chipmaker0304@gmail.com>
2021-08-21 23:08:54 +0900

    implement c++

--- Creative_algorithms_for_problem_solving_(intermediate)/Problem_26/c++/test.cpp
+++ Creative_algorithms_for_problem_solving_(intermediate)/Problem_26/c++/test.cpp
@@ -3,36 +3,35 @@
 #include <tuple>
 #include <stdlib.h>
 
-int prevAIndex, prevBIndex;
 int maxDist = 98765431;
 
+std::pair<int, int> s = {1, 1};
+std::pair<int, int> e = {6, 6};
+
 std::vector<std::pair<int, int>> tc = {
-	{1, 1},
 	{3, 5},
 	{5, 5},
-	{2, 3},
-	{6, 6}
+	{2, 3}
 };
 
-int distance(int a, int b){
-	int xd = abs(std::get<0>(tc[a]) -std::get<0>(tc[b]));
-	int yd = abs(std::get<1>(tc[a]) -std::get<1>(tc[b]));
-
-	return xd + yd;
+int distance(const std::pair<int, int> &a, const std::pair<int, int> &b){
+	return (abs(std::get<0>(a) - std::get<0>(b)) + abs(abs(std::get<1>(a) - std::get<1>(b))));
 }
 
-void solve(int idx, int a, int b){
+void solve(int idx, const std::pair<int, int> &a, const std::pair<int, int> &b, int d){
 	if(idx < tc.size()){
-		prevAIndex = a;
-		prevBIndex = b;
-		solve(idx+1, idx+1, b);
-		solve(idx+1, a, idx+1);
+		solve(idx+1, tc[idx], b, d+distance(a, tc[idx]));
+		solve(idx+1, a, tc[idx], d+distance(b, tc[idx]));
 	}
 	else{
-		
+		if(d < maxDist){
+			maxDist = d;
+		}
 	}
 }
 
 int main(){
+	solve(0, s, e, 0);
+	std::cout << maxDist << std::endl;
 	return 0;
 }

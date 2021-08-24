b17e6f43604c7dec3b62465ca8daa99abf18bab1
heesoon <chipmaker0304@gmail.com>
2021-08-24 12:39:03 +0900

    implement c++

--- Creative_algorithms_for_problem_solving_(intermediate)/Problem_31/c++/test.cpp
+++ Creative_algorithms_for_problem_solving_(intermediate)/Problem_31/c++/test.cpp
@@ -1,116 +1,37 @@
 #include <iostream>
 #include <vector>
-#include <tuple>
+#include <cstdlib>
 
-const int N = 11;
-int value[N][N];
-int bVisited[N][N];
-int result = 0x7fffffff;
+std::vector<int> pipe = {0, 12, 50, 81};
+std::vector<int> car = {27, 73};
+int visited[12];
+long minValue = 987654321;
 
-std::vector<std::tuple<int, int, int>> data ={
-	{1, 2, 47},
-	{1, 3, 69},
-	{2, 4, 57},
-	{2, 5, 124},
-	{3, 4, 37},
-	{3, 5, 59},
-	{3, 6, 86},
-	{4, 6, 27},
-	{4, 7, 94},
-	{5, 7, 21},
-	{6, 7, 40},
-};
-
-std::vector<int> map[N];
-
-void debug()
-{
-	for(int a = 0; a < N; a++)
-	{
-		for(int b = 0; b < N; b++)
-		{
-			std::cout.width(4);
-			//std::cout.fill('*');
-			std::cout << value[a][b];
-		}
-		std::cout << std::endl;
-	}
-	
-    std::cout << std::endl;
-	for(int a = 0; a < N; a++)
-	{
-        std::cout << a << " :";
-		for(int b = 0; b < map[a].size(); b++)
-		{
-			std::cout.width(2);
-			//std::cout.fill('*');
-			std::cout << map[a][b];
-		}
-		std::cout << std::endl;
-	}
-}
-
-void solve(int a, int d, int n)
-{
-	if(a == n)
-	{
-		if(result > d)
-		{
-			result = d;
-		}
-		
+void f(int a, int b, int v){
+	if(a == pipe.size()+1){
 		return;
 	}
 
-	for(int i = 0; i < map[a].size(); i++)
-	{
-		int ni = map[a][i];
+	if(a > 0 && b > 0 && minValue > v){
+		minValue = v;
+	}
+    
+    std::cout << a << ", " << b << ", " << v << std::endl;
 
-		if(bVisited[a][ni] == 0 && bVisited[ni][a] == 0)
-		{
-			bVisited[a][ni] = 1;
-            bVisited[ni][a] = 1;
-			solve(map[a][i], d + value[a][ni], n);
-			bVisited[a][ni] = 0;
-            bVisited[ni][a] = 0;
+	for(int i = 1; i <= pipe.size(); i++){
+		visited[a+1] = 1;
+		if(visited[i] == 0){
+			visited[i] = 1;
+			int dist = abs(pipe[a+1] - 27) + abs(pipe[i] - 73);
+			f(a+1, i, v+dist);
+			visited[i] = 0;
 		}
+		visited[a+1] = 0;
 	}
 }
 
-int main()
-{
-	int n = 8;
-/*	
-	std::cout << "Enter n : ";
-	std::cin >> n;
-	std::cout << std::endl;
-*/	
-	int m = 11;
-/*
-	std::cout << "Enter m : ";
-	std::cin >> m;
-	std::cout << std::endl;	
-*/
-
-	// update map by data
-	//for(std::vector<std::tuple<int, int, int> >::size_type i = 0; i < data.size(); i++)
-	for(int i = 0; i < m; i++)
-	{
-		int a, b, d;
-		auto& t = data[i];
-		std::tie(a, b, d) = t;
-
-		value[a][b] = d;
-		map[a].push_back(b);
-		value[b][a] = d;
-		map[b].push_back(a);		
-	}
-	
-	//debug();
-	
-	solve(1, 0, n-1);
-	
-	std::cout << result << std::endl;
-	
-    return 0;
+int main(){
+	f(0, 0, 0);
+	std::cout << minValue << std::endl;
+	return 0;
 }

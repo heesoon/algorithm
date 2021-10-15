2ae70c7392c69f3d52607f380df57420876f99eb
unknown <chipmaker0304@gmail.com>
2021-10-15 23:47:22 +0900

    complete implentation

--- Programmers/Problem_04/c++/test.cpp
+++ Programmers/Problem_04/c++/test.cpp
@@ -1,40 +1,61 @@
 #include <iostream>
-#include <vector>
 #include <string>
-#include <tuple>
-#include <queue>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
 
-auto cmp = [](const std::tuple<int, int> &a, const std::tuple<int, int> &b){
-    return std::get<1>(a) < std::get<1>(b);
-};
+using namespace std;
 
-template<typename T>
-void print_queue(T q) { // NB: pass by value so the print uses a copy
-    while(!q.empty()) {
-        std::tuple<int,int> a = q.top();
-        std::cout << std::get<0>(a) << ", " << std::get<1>(a) << std::endl;
-        q.pop();
+vector<int> solution(vector<string> genres, vector<int> plays) {
+    vector<int> answer;
+
+    std::unordered_map<std::string, int> best_genre;
+    std::unordered_map<std::string, std::vector<std::tuple<int, int>>> best_genre_play;
+
+    for(std::vector<std::string>::size_type i = 0; i < genres.size(); i++){
+        best_genre[genres[i]] += plays[i];
+        best_genre_play[genres[i]].push_back({i, plays[i]});
     }
-    std::cout << '\n';
+
+    std::vector<std::tuple<std::string, int>> vbest_genre(best_genre.begin(), best_genre.end());
+    std::sort(vbest_genre.begin(), vbest_genre.end(), [](const std::tuple<std::string, int> &a, const std::tuple<std::string, int> &b){
+        return std::get<1>(a) > std::get<1>(b);
+    });
+
+    for(auto& map : best_genre_play){
+        std::sort(std::get<1>(map).begin(), std::get<1>(map).end(), [](const std::tuple<int, int> &a, const std::tuple<int, int> &b){
+            return std::get<1>(a) > std::get<1>(b);
+        });
+    }
+
+    for(const auto& sorted_genre : vbest_genre){
+        int cnt = 0;
+        for(const auto& play : best_genre_play[std::get<0>(sorted_genre)]){
+            if(cnt > 1){
+                break;
+            }
+            answer.push_back(std::get<0>(play));
+            cnt++;
+        }
+    }
+
+    return answer;
 }
 
-void test_case_1(){
+void tc1(){
     std::vector<std::string> genres = {"classic", "pop", "classic", "classic", "pop"};
     std::vector<int> plays = {500, 600, 150, 800, 2500};
 
-
-    std::priority_queue<std::tuple<int, int>, std::vector<std::tuple<int, int>>, decltype(cmp)> pq(cmp);
-
-    int cnt = 0;
-    for(auto ve : plays){
-        pq.push({cnt, ve});
-        cnt++;
+    std::vector<int> answer = solution(genres, plays);
+    if(answer == std::vector<int>({4, 1, 3, 0})){
+        std::cout << "Success" << std::endl;
     }
-
-    print_queue(pq);
+    else{
+        std::cout << "Fail" << std::endl;
+    }
 }
 
 int main(){
-    test_case_1();
+    tc1();
     return 0;
 }

3039dc251c5d9bed7d0a5e8844ef1ced79eaa57b
unknown <chipmaker0304@gmail.com>
2021-10-15 20:41:42 +0900

    implementation code

--- Programmers/Problem_03/c++/test.cpp
+++ Programmers/Problem_03/c++/test.cpp
@@ -6,12 +6,23 @@
 using namespace std;
 
 int solution(vector<vector<string>> clothes) {
-    int answer = 0;
-    return answer;
+    int answer = 1;
+    std::unordered_map<std::string, int> map;
+
+    for(const auto& clothe_arr : clothes){
+        std::string key = clothe_arr[1];
+        map[key] += 1;
+    }
+
+    for(const auto& pair : map){
+        answer = answer * (pair.second + 1);
+    }
+
+    return answer - 1;
 }
 
 void tc1(){
-    std::vector<std::vector> clothes = {{"yellowhat", "headgear"}, {"bluesunglasses", "eyewear"}, {"green_turban", "headgear"}};
+    std::vector<std::vector<std::string>> clothes = {{"yellowhat", "headgear"}, {"bluesunglasses", "eyewear"}, {"green_turban", "headgear"}};
     if(solution(clothes) == 5){
         std::cout << "Success" << std::endl;
     }
@@ -21,7 +32,7 @@ void tc1(){
 }
 
 void tc2(){
-    std::vector<std::vector> clothes = {{"crowmask", "face"}, {"bluesunglasses", "face"}, {"smoky_makeup", "face"}};
+    std::vector<std::vector<std::string>> clothes = {{"crowmask", "face"}, {"bluesunglasses", "face"}, {"smoky_makeup", "face"}};
     if(solution(clothes) == 3){
         std::cout << "Success" << std::endl;
     }

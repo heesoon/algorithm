1f4eae7ace21ba28bf3c979a2b95caf252f36633
heesoon <chipmaker0304@gmail.com>
2021-10-21 00:16:42 +0900

    add implementation

--- Programmers/Problem_06/c++/test.cpp
+++ Programmers/Problem_06/c++/test.cpp
@@ -1,10 +1,46 @@
-#include <string>
+#include <iostream>
 #include <vector>
+#include <tuple>
+#include <deque>
 
 using namespace std;
 
 int solution(vector<int> priorities, int location) {
     int answer = 0;
+    int cnt = 0;
+    bool is_higher_priority = false;
+
+    std::deque<std::tuple<int, int>> printer;
+    for(auto const &p : priorities){
+        printer.push_back({cnt++, p});
+    }
+
+    cnt = 1;
+    while(!printer.empty()){
+        is_higher_priority = false;
+        auto front = printer.front();
+        printer.pop_front();
+
+        for(const auto& pair : printer){
+            if(std::get<1>(front) < std::get<1>(pair)){
+                is_higher_priority = true;
+            }
+        }
+
+        if(is_higher_priority == true){
+            printer.push_back(front);
+        }
+        else{
+            if(std::get<0>(front) == location){
+                break;
+            }
+            else{
+                cnt++;
+            }
+        }
+    }
+    
+    answer = cnt;
     return answer;
 }
 

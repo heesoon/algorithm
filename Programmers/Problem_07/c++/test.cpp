23b404663f99ba83c8c3f7756f9aa75e879a15df
heesoon <chipmaker0304@gmail.com>
2021-10-22 01:04:08 +0900

    complete implement

--- Programmers/Problem_07/c++/test.cpp
+++ Programmers/Problem_07/c++/test.cpp
@@ -1,10 +1,45 @@
+#include <iostream>
 #include <string>
 #include <vector>
+#include <deque>
 
 using namespace std;
 
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
     int answer = 0;
+    int total_weight = 0;
+    std::deque<int> queue;
+
+    for(int i = 0; i < bridge_length; i++){
+        queue.push_back(0);
+    }
+
+    while(queue.empty() == false || truck_weights.empty() == false){
+        if(truck_weights.empty() == false){
+            int vfront = *truck_weights.begin();
+            int qfront = queue.front();
+
+            if(total_weight + vfront -qfront <= weight){
+                total_weight += vfront;
+                truck_weights.erase(truck_weights.begin());
+
+                total_weight -= qfront;
+                queue.pop_front();
+                queue.push_back(vfront);
+            }
+            else{
+                total_weight -= qfront;
+                queue.pop_front();
+                queue.push_back(0);
+            }
+        }
+        else{
+            queue.pop_front();
+        }
+
+        answer++;
+    }
+
     return answer;
 }
 

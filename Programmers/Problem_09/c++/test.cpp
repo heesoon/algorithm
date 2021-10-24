51a31107849044a7b39830a2ff85e6b69029ce4f
heesoon <chipmaker0304@gmail.com>
2021-10-24 23:26:14 +0900

    complte codes

--- Programmers/Problem_09/c++/test.cpp
+++ Programmers/Problem_09/c++/test.cpp
@@ -1,10 +1,34 @@
+#include <iostream>
 #include <string>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
 int solution(vector<int> scoville, int K) {
     int answer = 0;
+    std::priority_queue<int, std::vector<int>, std::greater<int>> pq(scoville.begin(), scoville.end());
+
+    while(pq.size() >= 2){
+        if(pq.top() >= K){
+            break;
+        }
+        else{
+            int A = pq.top();
+            pq.pop();
+            int B = pq.top();
+            pq.pop();
+
+            pq.push(A + B*2);
+        }
+
+        answer++;
+    }
+
+    if(pq.top() < K){
+        return -1;
+    }
+
     return answer;
 }
 
@@ -12,7 +36,7 @@ void tc1(){
     std::vector<int> scoville = {1, 2, 3, 9, 10, 12};
 	int K = 7;
 
-    int answer = solution(prices);
+    int answer = solution(scoville, K);
     if(answer == 2){
         std::cout << "Success" << std::endl;
     }

4c1b7a2272260342fa5189ae21367da9104ae96d
heesoon <chipmaker0304@gmail.com>
2021-10-27 13:58:36 +0900

    add intermediat result

--- Programmers/Problem_11/c++/test.cpp
+++ Programmers/Problem_11/c++/test.cpp
@@ -1,10 +1,52 @@
+#include <iostream>
 #include <string>
 #include <vector>
+#include <queue>
+#include <sstream>
 
 using namespace std;
 
 vector<int> solution(vector<string> operations) {
-    vector<int> answer;
+    vector<int> answer{0, 0};
+    std::priority_queue<int, std::vector<int>, std::greater<int>> ipq;
+    std::priority_queue<int, std::vector<int>> dpq;
+    int cnt = 0;
+
+    for(const auto &str : operations){
+        int num;
+        char cmd = str[0];
+        std::stringstream numStr(str.substr(1));
+        numStr >> num;
+
+        switch(cmd){
+            case 'I':
+                ipq.push(num);
+                dpq.push(num);
+                cnt++;
+                break;
+            case 'D':
+                if(cnt > 0){
+                    if(num == -1){
+                        dpq.pop();
+                    }
+                    else if(num == 1){
+                        ipq.pop();
+                    }
+                    cnt--;
+                }
+                break;
+        }
+    }
+
+    if(cnt == 0){
+        answer[0] = 0;
+        answer[1] = 0;
+    }
+    else{
+        answer[0] = dpq.top();
+        answer[1] = ipq.top();
+    }
+
     return answer;
 }
 

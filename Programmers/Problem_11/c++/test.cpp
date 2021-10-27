10bd6507dad5de7927fc0f36ade39459d3b3b4ab
heesoon <chipmaker0304@gmail.com>
2021-10-27 15:32:42 +0900

    complete code

--- Programmers/Problem_11/c++/test.cpp
+++ Programmers/Problem_11/c++/test.cpp
@@ -27,14 +27,20 @@ vector<int> solution(vector<string> operations) {
             case 'D':
                 if(cnt > 0){
                     if(num == -1){
-                        dpq.pop();
+                        ipq.pop();
                     }
                     else if(num == 1){
-                        ipq.pop();
+                        dpq.pop();
                     }
                     cnt--;
                 }
                 break;
+        }
+
+        if(cnt == 0)
+        {
+            while(!ipq.empty()) ipq.pop();
+            while(!dpq.empty()) dpq.pop();
         }
     }
 
@@ -74,8 +80,52 @@ void tc2(){
     }
 }
 
+void tc3(){
+     std::vector<std::string> operations = {"I 4", "I 3", "I 2", "I 1", "D 1", "D 1", "D -1", "D -1", "I 5", "I 6"};
+
+    std::vector<int> answer = solution(operations);
+    if(answer == std::vector<int>({6, 5})){
+        std::cout << "Success" << std::endl;
+    }
+    else{
+        std::cout << "Fail" << std::endl;
+    }
+}
+
+
 int main(){
     tc1();
 	tc2();
+	tc3();    
     return 0;
 }
+
+/*
+#include <string>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+vector<int> solution(vector<string> arguments) {
+    vector<int> answer;
+    multiset<int> que;
+    multiset<int>::iterator iter;
+    string sub;
+
+    for(auto s : arguments) {
+        sub =s.substr(0, 2);
+        if(sub=="I ") que.insert(stoi(s.substr(2,s.length()-2))); 
+        else if(s.substr(2,1)=="1"&&que.size()>0) { que.erase(--que.end()); }
+        else if(que.size()>0) { que.erase(que.begin()); }
+    }
+
+    if(que.size()==0) { answer.push_back(0); answer.push_back(0); }
+    else { 
+        iter = --que.end(); answer.push_back(*iter); 
+        iter = que.begin(); answer.push_back(*iter);
+    }
+
+    return answer;
+}
+*/

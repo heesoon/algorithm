4076611f3dca157289083de3948884ad6c871ff5
heesoon <chipmaker0304@gmail.com>
2021-10-28 17:11:41 +0900

    add intermediate result

--- Programmers/Problem_17/c++/test.cpp
+++ Programmers/Problem_17/c++/test.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <string>
 #include <vector>
 
@@ -5,6 +6,33 @@ using namespace std;
 
 vector<int> solution(int brown, int yellow) {
     vector<int> answer;
+    std::vector<std::vector<int>> brown_cd;
+    std::vector<std::vector<int>> yellow_cd;
+
+    for(int i = 1; i < brown; i++){
+        if(brown % i == 0){
+            //std::cout << i << ", " << brown/i << std::endl;
+            brown_cd.push_back({i, brown/i});
+        }
+    }
+
+    for(int i = 1; i < yellow; i++){
+        if(yellow % i == 0){
+            yellow_cd.push_back({i, yellow/i});
+        }
+    }
+
+    std::cout << brown_cd.size() << ", " <<  yellow_cd.size() << std::endl;
+    for(size_t i = 2; i < brown_cd.size(); i++){
+        for(size_t j = 0; j < yellow_cd.size(); j++){
+            std::cout << brown_cd[i][0] << ", " << yellow_cd[j][0] + 2 << std::endl;
+            if((brown_cd[i][0] >= yellow_cd[j][0] + 2) && (brown_cd[i][1] >= yellow_cd[j][1] + 2)){
+                answer.push_back(brown_cd[i][0]);
+                answer.push_back(brown_cd[i][1]);
+            }
+        }
+    }
+
     return answer;
 }
 

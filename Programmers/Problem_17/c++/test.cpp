2019adcd51d5f281c1163203a041f8456ead11e8
heesoon <chipmaker0304@gmail.com>
2021-10-29 10:54:29 +0900

    add complete code

--- Programmers/Problem_17/c++/test.cpp
+++ Programmers/Problem_17/c++/test.cpp
@@ -6,30 +6,31 @@ using namespace std;
 
 vector<int> solution(int brown, int yellow) {
     vector<int> answer;
-    std::vector<std::vector<int>> brown_cd;
-    std::vector<std::vector<int>> yellow_cd;
 
-    for(int i = 1; i < brown; i++){
-        if(brown % i == 0){
-            //std::cout << i << ", " << brown/i << std::endl;
-            brown_cd.push_back({i, brown/i});
+    int rect = brown + yellow;
+
+    for(int i = 1; i <= rect/2; i++){
+        int x = 0;
+        int y = 0;
+
+        if(answer.empty() == false){
+            break;
         }
-    }
 
-    for(int i = 1; i < yellow; i++){
-        if(yellow % i == 0){
-            yellow_cd.push_back({i, yellow/i});
+        if(rect % i == 0){
+            y = i;
+            x = rect/i;
         }
-    }
+        for(int j = 1; j <= yellow; j++){
+            if(yellow % j == 0){
+                int ny = j;
+                int nx = yellow/j;
 
-    std::cout << brown_cd.size() << ", " <<  yellow_cd.size() << std::endl;
-    for(size_t i = 2; i < brown_cd.size(); i++){
-        for(size_t j = 0; j < yellow_cd.size(); j++){
-            std::cout << brown_cd[i][0] << ", " << yellow_cd[j][0] + 2 << std::endl;
-            if((brown_cd[i][0] >= yellow_cd[j][0] + 2) && (brown_cd[i][1] >= yellow_cd[j][1] + 2)){
-                answer.push_back(brown_cd[i][0]);
-                answer.push_back(brown_cd[i][1]);
-            }
+                if(y >= ny + 2 && x >= nx + 2){
+                    answer.push_back(x);
+                    answer.push_back(y);
+                }
+            }            
         }
     }
 
@@ -51,7 +52,7 @@ void tc1(){
 
 void tc2(){
     int brown = 8;
-	int yellow = 2;
+	int yellow = 1;
 
     std::vector<int> answer = solution(brown, yellow);
     if(answer == std::vector<int>({3, 3})){
@@ -81,3 +82,50 @@ int main(){
 	tc3();
     return 0;
 }
+
+/*
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int brown, int red) {
+
+    int len = brown / 2 + 2;
+
+    int w = len - 3;
+    int h = 3;
+
+    while(w >= h){
+        if(w * h == (brown + red)) break;
+
+        w--;
+        h++;
+    }
+
+    return vector<int>{w, h};
+}
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int brown, int yellow) {
+    vector<int> answer;
+
+    int x=2;
+    int y=brown/2-x;
+
+    while(1){
+        if((x-1)*(y-1)==yellow)
+            break;
+        x++;
+        y--;
+    }
+
+    answer.push_back(y+1);
+    answer.push_back(x+1);
+    return answer;
+}
+*/

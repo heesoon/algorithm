7772528a87a7dfa4a6b5d3a1a2b72f0ae710c55f
heesoon <chipmaker0304@gmail.com>
2021-10-29 13:37:10 +0900

    complete code

--- Programmers/Problem_18/c++/test.cpp
+++ Programmers/Problem_18/c++/test.cpp
@@ -1,10 +1,53 @@
+#include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int solution(int n, vector<int> lost, vector<int> reserve) {
     int answer = 0;
+    std::vector<int> have(n, 1);
+
+    for(const auto idx : lost){
+        have[idx - 1] -= 1;
+    }
+
+    for(const auto idx : reserve){
+        have[idx - 1] += 1;
+    }
+
+    for(int i = 0; i < n; i++){
+        if(have[i] == 0){
+            if( i == 0){
+                if(have[i+1] > 1){
+                    have[i+1] -= 1;
+                    have[i] += 1;
+                }
+            }
+            else if(i == n -1){
+                 if(have[i-1] > 1){
+                    have[i-1] -= 1;
+                    have[i] += 1;
+                }               
+            }
+            else{
+                if(have[i-1] > 1){
+                    have[i-1] -= 1;
+                    have[i] += 1;
+                }
+                else if(have[i+1] > 1){
+                    have[i+1] -= 1;
+                    have[i] += 1;
+                }
+            }
+        }
+    }
+
+    answer = std::count_if(have.begin(), have.end(), [](int item){
+        return item >= 1;
+    });
+
     return answer;
 }
 
@@ -56,3 +99,28 @@ int main(){
 	tc3();
     return 0;
 }
+
+/*
+#include <string>
+#include <vector>
+
+using namespace std;
+int student[35];
+int solution(int n, vector<int> lost, vector<int> reserve) {
+    int answer = 0;
+    for(int i : reserve) student[i] += 1;
+    for(int i : lost) student[i] += -1;
+    for(int i = 1; i <= n; i++) {
+        if(student[i] == -1) {
+            if(student[i-1] == 1) 
+                student[i-1] = student[i] = 0;
+            else if(student[i+1] == 1) 
+                student[i] = student[i+1] = 0;
+        }
+    }
+    for(int i  = 1; i <=n; i++)
+        if(student[i] != -1) answer++;
+
+    return answer;
+}
+*/

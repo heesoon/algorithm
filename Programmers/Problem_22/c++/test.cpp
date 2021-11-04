b865d48c49728f66014d2e3360b4de866844d211
heesoon <chipmaker0304@gmail.com>
2021-11-04 17:22:33 +0900

    intermediate result

--- Programmers/Problem_22/c++/test.cpp
+++ Programmers/Problem_22/c++/test.cpp
@@ -1,10 +1,43 @@
+#include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
+int getParents(const std::vector<int> &db, int node){
+    return db[node];
+}
+
 int solution(int n, vector<vector<int>> costs) {
     int answer = 0;
+
+    std::vector<int> parentDB;
+    for(size_t i = 0; i < costs.size(); i++){
+        parentDB.emplace_back(i);
+    }
+
+    std::sort(costs.begin(), costs.end(), [](const std::vector<int> &a, const std::vector<int> &b){
+        return a[2] < b[2];
+    });
+
+    for(const auto &v : costs){
+        int a = std::min(v[0], v[1]);
+        int b = std::max(v[0], v[1]);
+        int c = v[2];
+
+        int pa = getParents(parentDB, a);
+        int pb = getParents(parentDB, b);
+
+        if(pa == pb){
+            continue;
+        }
+        else{
+            parentDB[b] = a;
+            answer += c;
+        }
+    }
+
     return answer;
 }
 
@@ -12,7 +45,7 @@ void tc1(){
     int n = 4;
 	std::vector<std::vector<int>> costs = {{0,1,1}, {0,2,2}, {1,2,5}, {1,3,1}, {2,3,8}};
 
-    int = solution(n, costs);
+    int answer = solution(n, costs);
     if(answer == 4){
         std::cout << "Success" << std::endl;
     }

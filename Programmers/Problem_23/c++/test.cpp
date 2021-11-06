c0d18cd78a495eeaafc6e7d5048c6da694ebb606
unknown <chipmaker0304@gmail.com>
2021-11-07 01:05:23 +0900

    complete code

--- Programmers/Problem_23/c++/test.cpp
+++ Programmers/Problem_23/c++/test.cpp
@@ -1,10 +1,29 @@
+#include <iostream>
+#include <algorithm>
 #include <string>
 #include <vector>
 
 using namespace std;
 
 int solution(vector<vector<int>> routes) {
-    int answer = 0;
+    int answer = 1;
+
+    std::sort(routes.begin(), routes.end(), [](const std::vector<int> &a, const std::vector<int> &b){
+        return a[0] < b[0];
+    });
+
+    int cpos = routes[0][1];
+
+    for(int i = 1; i < routes.size(); i++){
+        if(routes[i][1] < cpos){
+            cpos = routes[i][1];
+        }
+        else if(routes[i][0] > cpos){
+            answer++;
+            cpos = routes[i][1];
+        }
+    }
+
     return answer;
 }
 
@@ -24,3 +43,24 @@ int main(){
     tc1();
     return 0;
 }
+
+/*
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool cmp(vector<int> a, vector<int> b) { return a[1] < b[1]; }
+
+int solution(vector<vector<int>> routes) {
+    int answer = 0;
+    int limit = -30001;
+    sort(routes.begin(), routes.end(), cmp);
+    for(int i = 0; i < routes.size(); i++){
+        if(limit < routes[i][0]){
+            answer++;
+            limit = routes[i][1];
+        }
+    }
+    return answer;
+}
+*/

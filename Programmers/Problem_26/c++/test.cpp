f1c926960053487e45a6a297bb5669389e90b35b
heesoon <chipmaker0304@gmail.com>
2021-11-09 15:31:30 +0900

    complete code

--- Programmers/Problem_26/c++/test.cpp
+++ Programmers/Problem_26/c++/test.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+#if 0
 void print(const std::vector<std::vector<int>> &ar){
     for(int i = 0; i < ar.size(); i++){
         for(int j = 0; j < ar[0].size(); j++){
@@ -12,6 +13,7 @@ void print(const std::vector<std::vector<int>> &ar){
         std::cout << std::endl;
     }
 }
+#endif
 
 int solution(int m, int n, vector<vector<int>> puddles) {
     int answer = 0;
@@ -26,24 +28,37 @@ int solution(int m, int n, vector<vector<int>> puddles) {
     }
 
     for(const auto v : puddles){
-        dp[v[1]][v[0]] = 0;
+        int y = v[1];
+        int x = v[0];
+
+        dp[y][x] = 0;
+
+        // x == 1
+        if(x == 1){
+            for(int i = y; i <= n; i++){
+                dp[i][x] = 0;
+            }
+        }
+
+        // y == 1
+        if(y == 1){
+            for(int i = x; i <= m; i++){
+                dp[y][i] = 0;
+            }
+        }    
     }
 
-    for(size_t y = 1; y <= n; y++){
-        for(size_t x = 1; x <= m; x++){
+    for(size_t y = 2; y <= n; y++){
+        for(size_t x = 2; x <= m; x++){
             if(dp[y][x] == 0){
                 continue;
             }
 
-            dp[y][x] = dp[y-1][x] + dp[y][x-1];
+            dp[y][x] = (dp[y-1][x] + dp[y][x-1]) % 1000000007;
         }
     }
 
-    answer = dp[n][m] % 1'000'000'007;
-    //std::cout << answer << std::endl;
-    
-    //print(dp);
-    return answer;
+    return dp[n][m];
 }
 
 void tc1(){
@@ -64,3 +79,9 @@ int main(){
     tc1();
     return 0;
 }
+
+// https://hochoon-dev.tistory.com/entry/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EB%93%B1%EA%B5%A3%EA%B8%B8-c
+// x = 1, y = 1일 때, 고려를 못함.
+
+// 완전탐색을 DP로 변환시 활용 가능 코드
+// https://dailyheumsi.tistory.com/25

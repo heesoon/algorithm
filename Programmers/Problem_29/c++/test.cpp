2956d2fede6f784dac57b6ae3d13568f7b18acac
heesoon <chipmaker0304@gmail.com>
2021-11-11 16:34:46 +0900

    intermediate result

--- Programmers/Problem_29/c++/test.cpp
+++ Programmers/Problem_29/c++/test.cpp
@@ -1,10 +1,32 @@
+#include <iostream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
+bool dfs(int idx, vector<vector<int>> computers){
+    if(computers[idx][idx] == 0) return false;
+
+    computers[idx][idx] = 0;
+
+    for(int i = 0; i < computers.size(); i++){
+        if(computers[idx][i] == 1){
+            dfs(i, computers);
+        } 
+    }
+
+    return true;
+}
+
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
+
+    for(int i = 0; i < n; i++){
+        if(computers[i][i] && dfs(i, computers)){
+            answer++;
+        }
+    }
+
     return answer;
 }
 

24b456c70e6150de0885d8eed3fedee7be7d8031
heesoon <chipmaker0304@gmail.com>
2021-11-11 13:45:38 +0900

    complete code

--- Programmers/Problem_28/c++/test.cpp
+++ Programmers/Problem_28/c++/test.cpp
@@ -1,10 +1,34 @@
+#include <iostream>
 #include <string>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
 int solution(vector<int> numbers, int target) {
     int answer = 0;
+    std::queue<std::vector<int>> vq;
+    vq.push({0, numbers[0]});
+    vq.push({0, -numbers[0]});
+
+    while(!vq.empty()){
+        auto v = vq.front();
+        vq.pop();
+        int idx = v[0];
+        int value = v[1];
+
+        if(value == target && idx == numbers.size()-1){
+            answer++;
+        }
+
+        if(idx < numbers.size() - 1){
+            idx++;
+            vq.push({idx, value+numbers[idx]});
+            vq.push({idx, value-numbers[idx]});
+        }
+    }
+
+    //std::cout << answer << std::endl;
     return answer;
 }
 

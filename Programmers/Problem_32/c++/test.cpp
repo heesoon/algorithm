adb8765141257cae33bd769dea6ad22db53c6038
heesoon <chipmaker0304@gmail.com>
2021-11-15 11:03:05 +0900

    complete code

--- Programmers/Problem_32/c++/test.cpp
+++ Programmers/Problem_32/c++/test.cpp
@@ -1,10 +1,32 @@
+#include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 long long solution(int n, vector<int> times) {
     long long answer = 0;
+    long long s = 1;
+    long long e = *max_element(times.begin(), times.end()) * (long long)n;
+
+    while(s <= e){
+        long long cnt = 0;
+        long long mid = (s + e)/2;
+
+        for(const auto &t : times){
+            cnt += mid/t;
+        }
+
+        if(cnt < n){
+            s = mid + 1;
+        }
+        else{
+            answer = mid;
+            e = mid - 1;
+        }
+    }
+
     return answer;
 }
 

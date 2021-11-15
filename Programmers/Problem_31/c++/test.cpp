66dad7899a4a0d9a20d4937782d575a359864b8e
heesoon <chipmaker0304@gmail.com>
2021-11-15 09:47:10 +0900

    complete code

--- Programmers/Problem_31/c++/test.cpp
+++ Programmers/Problem_31/c++/test.cpp
@@ -5,28 +5,35 @@
 
 using namespace std;
 
-void dfs(int cnt, std::string str, std::vector<int> &visited, std::vector<std::string> &answer, std::vector<std::string> &temp, vector<vector<string>> tickets){
+bool dfs(int cnt, std::string str, std::vector<int> &visited, std::vector<std::string> &answer, std::vector<std::string> &temp, vector<vector<string>> tickets){
+    bool retval = false;
     temp.push_back(str);
 
-    if(cnt >= tickets.size()){
+    if(cnt == tickets.size()){
         answer = temp;
-        return;
+        return true;
     }
 
+#if 0
     for(const auto &v : visited){
         std::cout << v << " ";
     }
     std::cout << std::endl;
+#endif
 
     for(vector<vector<string>>::size_type i = 0; i < tickets.size(); i++){
         if(str == tickets[i][0] && visited[i] == 0){
-            std::cout << " >> " << i << " , " << str << std::endl;
+            //std::cout << " >> " << i << " , " << str << std::endl;
             visited[i] = 1;
-            dfs(cnt+1, tickets[i][1], visited, answer, temp, tickets);
+            retval = dfs(cnt+1, tickets[i][1], visited, answer, temp, tickets);
             visited[i] = 0;
+            if(retval == true){
+                return true;
+            }
         }
     }
     temp.pop_back();
+    return retval;
 }
 
 vector<string> solution(vector<vector<string>> tickets) {
@@ -34,18 +41,21 @@ vector<string> solution(vector<vector<string>> tickets) {
     std::vector<int> visited(tickets.size(), 0);
     std::sort(tickets.begin(), tickets.end());
 
-#if 1
+#if 0
     for(const auto &v : tickets){
         std::cout << v[0] << " , " << v[1] << std::endl;
     }
     std::cout << std::endl;
 #endif
+    
     dfs(0, "ICN", visited, answer, temp, tickets);
 
+#if 0
     for(const auto &v : answer){
         std::cout << v << " ";
     }
     std::cout << std::endl;
+#endif
 
     return answer;
 }
@@ -75,7 +85,7 @@ void tc2(){
 }
 
 int main(){
-    //tc1();
+    tc1();
 	tc2();
     return 0;
 }

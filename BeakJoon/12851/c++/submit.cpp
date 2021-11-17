58b19f1cba72919c895dac864ae636e1f662df8e
heesoon <chipmaker0304@gmail.com>
2021-11-17 19:07:39 +0900

    complte code

--- BeakJoon/12851/c++/submit.cpp
+++ BeakJoon/12851/c++/submit.cpp
@@ -1,30 +1,28 @@
 #include <iostream>
-#include <string>
-#include <vector>
 #include <queue>
 
 using namespace std;
 
-#define LIMIT 100'000
+const int LIMIT = 100'000 + 1;;
+bool visited[LIMIT];
+int minPathCnt = 0;
+int minCost = 0;
 
-void solution(int N, int K) {
-    int next = 0;
-    int minCost = 0;
-    int minPathCnt = 0;
-    std::queue<std::vector<int>> Q;
-    std::vector<bool> visited(100'001, false);
-    Q.push({N, 0});
+int solution(int N, int K) {
+    
+    queue<pair<int,int>> Q;
+    Q.push(make_pair(N, 0));
     visited[N] = true;
 
     while(!Q.empty()){
-        auto front = Q.front();
-        int curr = front[0];
-        int depth = front[1];
+        int curr = Q.front().first;
+        int depth = Q.front().second;
         Q.pop();
 
-        visited[K] = false;
+        visited[curr] = true;
+        //visited[K] = false;
 
-        if(minCost == depth && curr == K){
+        if(minCost && minCost == depth && curr == K){
             minPathCnt++;
         }
 
@@ -32,36 +30,32 @@ void solution(int N, int K) {
             minCost = depth;
             minPathCnt++;
         }
-
-        next = curr - 1;
-        if(next >= 0 && visited[next] == false){
-            visited[next] = true;
-            Q.push({next, depth + 1});
+        
+        if(curr + 1 < LIMIT && visited[curr + 1] == false){
+            //visited[curr + 1] = true;
+            Q.push(make_pair(curr + 1, depth+1));
+        }
+        
+        if(curr - 1 >= 0 && visited[curr - 1] == false){
+            //visited[curr - 1] = true;
+            Q.push(make_pair(curr - 1, depth+1));
         }
 
-        next = curr + 1;
-        if(next <= LIMIT && visited[next] == false){
-            visited[next] = true;
-            Q.push({next, depth + 1});
-        }
-
-        next = curr*2;
-        if(next <= LIMIT && visited[next] == false){
-            visited[next] = true;
-            Q.push({next, depth + 1});
+        if(curr * 2 < LIMIT && visited[curr*2] == false){
+            //visited[curr*2] = true;
+            Q.push(make_pair(curr*2, depth+1));
         }
     }
 
-    std::cout << minCost << std::endl;
-    std::cout << minPathCnt << std::endl;
+    return minCost;
 }
 
 
 int main(){   
     int N, K;
-    std::cin >> N >> K;
+    cin >> N >> K;
 
-    solution(N, K);
-
+    cout << solution(N, K) << "\n";
+    cout << minPathCnt << std::endl;
     return 0;
 }

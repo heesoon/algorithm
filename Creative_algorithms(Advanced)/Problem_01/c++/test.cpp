31ee07a19b3f2ccf7e544719b3475c9024a5fa8e
heesoon <chipmaker0304@gmail.com>
2021-11-21 01:33:28 +0900

    upload code

--- Creative_algorithms(Advanced)/Problem_01/c++/test.cpp
+++ Creative_algorithms(Advanced)/Problem_01/c++/test.cpp
@@ -1,18 +1,46 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-long long solution(int n, int r) {
-    long long answer;
+const int MAX = 30 + 1;
 
-    return answer;
+long long solution(int n, int k) {
+    long long answer;
+    std::vector<std::vector<long long>> dp(MAX, std::vector<long long>(MAX, 0));
+    
+    if(n == k){
+        return 1;
+    }
+
+    if(k == 1){
+        return n;
+    }
+
+    dp[1][1] = 1;
+    dp[1][0] = 1;
+    for(int i = 2; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            if(i == j){
+                dp[i][j] = 1;
+            }
+            else if(j == 1){
+                dp[i][j] = i;
+            }
+            else if(i > j){
+                dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+            }
+        }
+    }
+
+    return answer = dp[n][k];
 }
 
 void tc1(){
     int n = 5;
-    int r = 1;
+    int k = 1;
 
-    long long answer = solution(n, r);
+    long long answer = solution(n, k);
     if(answer == 5){
         std::cout << "Success" << std::endl;
     }
@@ -23,9 +51,9 @@ void tc1(){
 
 void tc2(){
     int n = 10;
-    int r = 5;
+    int k = 5;
 
-    long long answer = solution(n, r);
+    long long answer = solution(n, k);
     if(answer == 252){
         std::cout << "Success" << std::endl;
     }

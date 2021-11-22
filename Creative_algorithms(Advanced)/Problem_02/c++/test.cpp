32265139a83ae01a1b8206745c0a737a58769064
heesoon <chipmaker0304@gmail.com>
2021-11-22 16:48:23 +0900

    update materials

--- Creative_algorithms(Advanced)/Problem_02/c++/test.cpp
+++ Creative_algorithms(Advanced)/Problem_02/c++/test.cpp
@@ -3,45 +3,18 @@
 
 using namespace std;
 
-const int MAX = 30 + 1;
+long long solution(int n, int m) {
+    long long answer = 0;
 
-long long solution(int n, int k) {
-    long long answer;
-    std::vector<std::vector<long long>> dp(MAX, std::vector<long long>(MAX, 0));
-    
-    if(n == k){
-        return 1;
-    }
-
-    if(k == 1){
-        return n;
-    }
-
-    dp[1][1] = 1;
-    dp[1][0] = 1;
-    for(int i = 2; i <= n; i++){
-        for(int j = 1; j <= i; j++){
-            if(i == j){
-                dp[i][j] = 1;
-            }
-            else if(j == 1){
-                dp[i][j] = i;
-            }
-            else if(i > j){
-                dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
-            }
-        }
-    }
-
-    return answer = dp[n][k];
+    return answer;
 }
 
 void tc1(){
-    int n = 5;
-    int k = 1;
+    int n = 8;
+    int m = 100;
 
-    long long answer = solution(n, k);
-    if(answer == 5){
+    long long answer = solution(n, m);
+    if(answer == 71){
         std::cout << "Success" << std::endl;
     }
     else{
@@ -49,25 +22,12 @@ void tc1(){
     }
 }
 
-void tc2(){
-    int n = 10;
-    int k = 5;
+void tc1(){
+    int n = 8;
+    int m = 2;
 
-    long long answer = solution(n, k);
-    if(answer == 252){
-        std::cout << "Success" << std::endl;
-    }
-    else{
-        std::cout << "Fail" << std::endl;
-    }
-}
-
-void tc3(){
-    int n = 29;
-    int k = 13;
-
-    long long answer = solution(n, k);
-    if(answer == 67863915){
+    long long answer = solution(n, m);
+    if(answer == 1){
         std::cout << "Success" << std::endl;
     }
     else{
@@ -78,6 +38,5 @@ void tc3(){
 int main(){
     tc1();
     tc2();
-    tc3();
     return 0;
 }

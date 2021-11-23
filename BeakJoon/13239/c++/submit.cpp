469d239f8da16e1a27539f8f2ea304ef888cc419
heesoon <chipmaker0304@gmail.com>
2021-11-23 13:10:58 +0900

    complete code

--- BeakJoon/13239/c++/submit.cpp
+++ BeakJoon/13239/c++/submit.cpp
@@ -1,70 +1,51 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 
 using namespace std;
 
-bool valid(int n, const std::vector<bool> &visited){
-    if(n < 0 || n > 100'000 || visited[n]){
-        return false;
+long long solution(int n, int k) {
+    std::vector<std::vector<long long>> dp(n+1, std::vector<long long>(n+1, 0));
+    dp[0][0] = 1;
+    dp[1][0] = 1;
+    dp[1][1] = 1;
+
+    if(n == k){
+        return 1;
     }
-
-    return true;
-}
-
-int solution(int N, int K) {
-    int answer = 0;
-    int next = 0;
-
-    std::priority_queue<std::vector<int>, std::vector<std::vector<int>>, std::greater<std::vector<int>>> Q;
-    //std::priority_queue<std::pair<int,int>, std::vector<std::pair<int,int>>, std::greater<std::pair<int,int>>> Q;
-    std::vector<bool> visited(100'001, false);
-    //Q.push(make_pair(0, N));
-    Q.push({0, N});
-    visited[N] = true;
-
-    while(!Q.empty()){
-        auto front = Q.top();
-        int depth = front[0];
-        int curr = front[1];
-        //int depth = Q.top().first;
-        //int curr = Q.top().second;        
-        Q.pop();
-
-        if(curr == K){
-            answer = depth;
-            break;
-        }
-
-        next = curr*2;
-        if(valid(next, visited)){
-            visited[next] = true;
-            Q.push({depth, next});
-            //Q.push(make_pair(depth, next));
-        }
-
-        next = curr - 1;
-        if(valid(next, visited)){
-            visited[next] = true;
-            Q.push({depth + 1, next});
-            //Q.push(make_pair(depth+1, next));
-        }
-
-        next = curr + 1;
-        if(valid(next, visited)){
-            visited[next] = true;
-            Q.push({depth + 1, next});
-            //Q.push(make_pair(depth+1, next));
+    else if(k == 1){
+        return n;
+    }
+    else{
+        for(int i = 2; i <= n; i++){
+            for(int j = 0; j <= k; j++){
+                if(i == j){
+                    dp[i][j] = 1;
+                }
+                else if(j == 0){
+                    dp[i][j] = 1;
+                }
+                else if(j == 1){
+                    dp[i][j] = i;
+                }
+                else if(i > j){
+                    dp[i][j] = (dp[i-1][j] + dp[i-1][j-1]) % (1000'000'000 + 7);
+                }
+            }
         }
     }
 
-    return answer;
+    return dp[n][k];
 }
 
-int main(){   
-    int N, K;
-    std::cin >> N >> K;
+int main(){
+    int t, n, k;
 
-    std::cout << solution(N, K);
+    std::cin >> t;
+
+    for(int i = 0; i < t; i++){
+        std::cin >> n >> k;
+        std::cout << solution(n, k) << std::endl;
+    }
+
     return 0;
 }

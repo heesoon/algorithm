da206646577d9cc8c646ff2a2d254ca5b643291f
heesoon <chipmaker0304@gmail.com>
2021-11-30 15:16:07 +0900

    upload soltuion

--- Creative_algorithms(Advanced)/Problem_11/c++/test.cpp
+++ Creative_algorithms(Advanced)/Problem_11/c++/test.cpp
@@ -2,41 +2,43 @@
 #include <vector>
 #include <algorithm>
 
-int N = 4;
-int W[101] = {2, 1, 3, 2, };
-int V[101] = {3, 2, 3, 2, };
-std::vector<std::vector<int>> DT(101, std::vector<int>(1001, -1));
+const int N = 100 + 1;
+const int C = 100 + 1;
+std::vector<int> usedMem;
+std::vector<int> cost;
+std::vector<std::vector<int>> DT(N, std::vector<int>(N*C, 0));
 
-int f(int idx, int r){
-#if 0    
-    if(idx == N){
-        return 0;
+int solution(int n, int m){
+    int answer = 0;
+    for(int i = 1; i < n; i++){
+        for(int j = 0; j <= N*C; j++){
+            if(j < cost[i]){
+                DT[i][j] = DT[i-1][j];
+            }
+            else{
+                DT[i][j] = std::max(DT[i-1][j], DT[i-1][j - cost[i]] + usedMem[i]);
+            }
+        }
     }
-    else if(W[idx] <= r){
-        return std::max(f(idx+1, r), f(idx+1, r-W[idx]) + V[idx]);
+
+    for(int i = 0; i <= N*C; i++){
+        if(DT[n-1][i] >= m){
+            break;
+        }
+
+        answer = i;
     }
-    else{
-        return f(idx+1, r);
-    }
-#else
-    if(DT[idx][r] != -1){
-        return DT[idx][r];
-    }
-    if(idx == N){
-        return DT[idx][r] = 0;
-    }
-    else if(W[idx] <= r){
-        return DT[idx][r] = std::max(f(idx+1, r), f(idx+1, r-W[idx]) + V[idx]);
-    }
-    else{
-        return DT[idx][r] = f(idx+1, r);
-    }
-#endif
+
+    return answer;
 }
 
 void tc1(){
-    int target = 5;
-    if(7 == f(0, target)){
+    int n = 5;
+    int m = 60;
+    usedMem.assign({30, 10, 20, 35, 40});
+    cost.assign({3, 0, 3, 5, 4});
+
+    if(6 == solution(n, m)){
         std::cout << "Success" << std::endl;
     }
     else{

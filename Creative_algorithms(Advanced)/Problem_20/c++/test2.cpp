ef60b07f86d8596f2102884cb3e22557e0ceff81
heesoon <chipmaker0304@gmail.com>
2021-12-13 13:17:10 +0900

    add intermediate result

--- Creative_algorithms(Advanced)/Problem_20/c++/test2.cpp
+++ Creative_algorithms(Advanced)/Problem_20/c++/test2.cpp
@@ -4,27 +4,38 @@
 
 std::vector<std::string> bridge;
 
-int solve(int y, int x, int c, std::string str){
-	int ans = 0;
+int solve(std::string str){
+	std::vector<std::vector<std::vector<int>>> DT(2, std::vector<std::vector<int>>(bridge.size(), std::vector<int>(str.size(), 0)));
 
-	if(c == str.size()){
-		return 1;
-	}
-
-	for(int i = x; i < bridge[0].size(); i++){
-		if(bridge[y][i] == str[c]){
-			ans += solve((y+1) % 2, i+1, c+1, str);
+	for(int i = 0; i < 2; i++){
+		for(int j = 0; j < bridge.size(); j++){
+			for(int k = 0; k < str.size(); k++){
+				if(bridge[i][j] == str[k]){
+					DT[i][j][k] = 1;
+				}
+			}
 		}
 	}
 
-	return ans;
+	for(int i = 0; i < 2; i++){
+		for(int j = 0; j < bridge.size(); j++){
+			for(int k = 1; k < str.size(); k++){
+				if(bridge[i][j] == str[k]){
+					DT[i][j][k] += DT[1-i][j][k-1];
+				}
+			}
+		}
+	}
+
+	return DT[0][bridge.size()-1][str.size()-1] + DT[1][bridge.size()-1][str.size()-1];
 }
 
 void tc1(){
 	int ans = 0;
 	bridge.assign({"RINGSR", "GRGGNS"});
 
-	ans = solve(0, 0, 0, "RGS") + solve(1, 0, 0, "RGS");
+	ans = solve("RGS");
+
 	if(3 == ans){
 		std::cout << "Success" << std::endl;
 	}
@@ -37,7 +48,7 @@ void tc2(){
 	int ans = 0;
 	bridge.clear();
 	bridge.assign({"SGNIRSGNIR", "GNIRSGNIRS"});
-	ans = solve(0, 0, 0, "RINGS") + solve(1, 0, 0, "RINGS");
+	ans = solve("RINGS");
 	if(0 == ans){
 		std::cout << "Success" << std::endl;
 	}
@@ -50,7 +61,7 @@ void tc3(){
 	int ans = 0;
 	bridge.clear();
 	bridge.assign({"GGGGRRRR", "IIIIGGGG"});
-	ans = solve(0, 0, 0, "GG") + solve(1, 0, 0, "GG");
+	ans = solve("GG");
 	if(16 == ans){
 		std::cout << "Success" << std::endl;
 	}

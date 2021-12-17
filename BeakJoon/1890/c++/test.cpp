8051fbd041ff9e56fc7aa433094071e1695731c6
unknown <chipmaker0304@gmail.com>
2021-12-17 23:42:28 +0900

    add another solution but memory overflow

--- BeakJoon/1890/c++/test.cpp
+++ BeakJoon/1890/c++/test.cpp
@@ -2,39 +2,29 @@
 #include <vector>
 #include <cstring>
 
-std::vector<std::vector<int>> map;
+int N;
+int map[101][101];
 long long cache[101][101];
 
 long long solution(int y, int x){ 
-    if(y >= map.size() || x >= map.size())      return 0;
-    if(y == map.size()-1 && x == map.size()-1)  return 1;
+    if(y >= N || x >= N)      return 0;
+    if(y == N-1 && x == N-1)  return 1;
 
     long long &ret = cache[y][x];
     if(ret != -1)   return ret;
     else            return ret = solution(y+map[y][x], x) + solution(y, x+map[y][x]);
 }
 
-void tc1(){
-    map.assign({
-        {2, 3, 3, 1},
-        {1, 2, 1, 3},
-        {1, 2, 3, 1},
-        {3, 1, 1, 0}
-    });
+int main(){   
+    std::cin >> N;    
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            std::cin >> map[i][j];
+        }
+    }
 
     std::memset(cache, -1, sizeof(cache));
+    std::cout << solution(0, 0) << std::endl;
 
-    if(3 == solution(0, 0)){
-        std::cout << "Success" << std::endl;
-    }
-    else{
-        std::cout << "Fail" << std::endl;
-    }
-
-    map.clear();
-}
-
-int main(){
-    tc1();
     return 0;
 }

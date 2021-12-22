26c86740e9f730dd6f2b5b28da3e4680f7b86535
heesoon <chipmaker0304@gmail.com>
2021-12-22 16:30:48 +0900

    complete code

--- BeakJoon/11722/c++/submit.cpp
+++ BeakJoon/11722/c++/submit.cpp
@@ -1,29 +1,29 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
-using namespace std;
+std::vector<int> S;
+std::vector<int> DT;
 
-const int MAX = 1000 + 1;
-
-int solution(int n) {
-    std::vector<int> dp(MAX, 0);
-
-    // 왜 dp[0] = 1로 넣어줘야지만 홀짝으로 분리시 정상값을 얻어옮은 생각 필요
-    dp[0] = 1;
-    dp[1] = 1;
-    dp[2] = 2;
-
-    for(int i = 3; i <= n; i++){
-        dp[i] = (dp[i-1] + dp[i-2]) % 10'007;
+int solve(int n){
+    for(int i = 0; i < n; i++){
+        std::cin >> S[i];
+        DT[i] = 1;
+        for(int j = 0; j < i; j++){
+            if(S[i] < S[j]){
+                DT[i] = std::max(DT[i], DT[j]+1);
+            }
+        }
     }
-
-    return dp[n];
+ 
+    return *std::max_element(DT.begin(), DT.end());
 }
 
 int main(){
     int n;
     std::cin >> n;
-
-    std::cout << solution(n) << std::endl;
+    DT.assign(n, 0);
+    S.assign(n, 0);
+    std::cout << solve(n) << std::endl;
     return 0;
 }

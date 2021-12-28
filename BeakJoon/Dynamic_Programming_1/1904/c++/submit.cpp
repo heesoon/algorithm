65664637ffdedec65b2316c224b45f8698c41146
heesoon <chipmaker0304@gmail.com>
2021-12-28 15:11:34 +0900

    reload

--- BeakJoon/Dynamic_Programming_1/1904/c++/submit.cpp
+++ BeakJoon/Dynamic_Programming_1/1904/c++/submit.cpp
@@ -5,18 +5,18 @@
 std::vector<int> DT;
 
 int solve(int n){
-    for(int i = 2; i < n; i++){
+    for(int i = 2; i <= n; i++){
         DT[i] = (DT[i-2] + DT[i-1]) % 15746;
     }
 
-    return DT[n-1];
+    return DT[n;
 }
 
 int main(){
     int n;
     std::cin >> n;
 
-    DT.assign(n, 0);
+    DT.assign(n+1, 0);
     DT[0] = 1;
     DT[1] = 1;
     std::cout << solve(n) << "\n";

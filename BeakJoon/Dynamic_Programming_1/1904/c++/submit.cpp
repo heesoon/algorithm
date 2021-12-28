af6c32593b010af06d78071fe6acd763c8674a8b
heesoon <chipmaker0304@gmail.com>
2021-12-28 15:10:07 +0900

    complete code

--- BeakJoon/Dynamic_Programming_1/1904/c++/submit.cpp
+++ BeakJoon/Dynamic_Programming_1/1904/c++/submit.cpp
@@ -1,34 +1,25 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 
-const int MAX = 21;
-int cache[MAX][MAX][MAX];
+std::vector<int> DT;
 
-int w(int a, int b, int c){
+int solve(int n){
+    for(int i = 2; i < n; i++){
+        DT[i] = (DT[i-2] + DT[i-1]) % 15746;
+    }
 
-    if(a <= 0 || b <= 0 || c <= 0)
-        return 1;
-
-    if(a > 20 || b > 20 || c > 20)
-        return w(20, 20, 20);
-
-    int &ret = cache[a][b][c];
-    if(ret != -1)
-        return ret;
-
-    if(a < b && b < c)
-        return ret = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c);
-    else
-        return ret = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1);
+    return DT[n-1];
 }
 
 int main(){
-    int a, b, c;
-    while(1){
-        std::fill((int*)cache, (int*)cache+(MAX*MAX*MAX), -1);
-        std::cin >> a >> b >> c;
-        if(a == -1 && b == -1 && c == -1) break;
-        std::cout << "w(" << a << ", " << b << ", " << c << ") = " << w(a, b, c) << std::endl;
-    }
+    int n;
+    std::cin >> n;
+
+    DT.assign(n, 0);
+    DT[0] = 1;
+    DT[1] = 1;
+    std::cout << solve(n) << "\n";
+
     return 0;
 }

03b7e1abd225774b6b1417e7e0653425a624d0f7
heesoon <chipmaker0304@gmail.com>
2021-12-30 20:16:38 +0900

    complete code

--- BeakJoon/Dynamic_Programming_1/1912/c++/submit.cpp
+++ BeakJoon/Dynamic_Programming_1/1912/c++/submit.cpp
@@ -2,57 +2,34 @@
 #include <vector>
 #include <algorithm>
 
-std::vector<int> LLIS;
-std::vector<int> RLIS;
-std::vector<int> S;
+std::vector<int> DT;
 
 int solve(int n){
-    int ans = 0;
+    int a;
+    for(int i = 1; i <= n; i++){
+        std::cin >> a;
+        if(i == 1){
+            DT[i] = a;
+            continue;
+        }
 
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < i; j++){
-            if(S[j] < S[i]){
-                RLIS[i] = std::max(RLIS[i], RLIS[j]+1);
-            }
+        if(DT[i-1] < 0){
+            DT[i] = a;
+        }
+        else{
+            DT[i] = DT[i-1] + a;
         }
     }
+    
+    auto begin = DT.begin();
+    std::advance(begin, 1);
 
-    for(int i = n-1; i >= 0; i--){
-        for(int j = n-1; j > i; j--){
-            if(S[j] < S[i]){
-                LLIS[i] = std::max(LLIS[i], LLIS[j]+1);
-            }
-        }
-    }
-
-#if 0
-    for(int i = 0; i < n; i++){
-        std::cout << RLIS[i] << " ";
-    }
-    std::cout << std::endl;
-    for(int i = 0; i < n; i++){
-        std::cout << LLIS[i] << " ";
-    }
-    std::cout << std::endl;
-#endif
-
-    for(int i = 0; i < n; i++){
-        ans = std::max(ans, RLIS[i] + LLIS[i] - 1);
-    }
-
-    return ans;
+    return *std::max_element(begin, DT.end());
 }
 
 int main(){
     int n;
     std::cin >> n;
-    LLIS.assign(n, 1);
-    RLIS.assign(n, 1);
-    S.assign(n, 0);
-
-    for(int i = 0; i < n; i++){
-        std::cin >> S[i];
-    }
-
+    DT.assign(n+1, 0);
     std::cout << solve(n) << std::endl;
 }

b0050a640e1406cf09d51d6a0bbd677155aa8a9e
heesoon <chipmaker0304@gmail.com>
2021-12-30 12:54:18 +0900

    complete code

--- BeakJoon/Dynamic_Programming_1/10844/c++/submit.cpp
+++ BeakJoon/Dynamic_Programming_1/10844/c++/submit.cpp
@@ -2,27 +2,40 @@
 #include <algorithm>
 
 const int MAX = 101;
-long long DT[MAX];
+long long DT[MAX][10];
 
 long long solve(int n){
-    DT[1] = DT[2] = DT[3] = 1;
-    DT[4] = DT[5] = 2;
-
-    for(int i = 6; i <= n; i++){
-        DT[i] = DT[i-1] + DT[i-5];
+    long long ans = 0;
+    for(int i = 1; i <= n; i++){
+        for(int j = 0; j < 10; j++){
+            if(i == 1){
+                if(j != 0) DT[i][j] = 1;
+            }
+            else{
+                if(j == 0){
+                    DT[i][j] = DT[i-1][j+1] % 1'000'000'000;
+                }
+                else if(j == 9){
+                    DT[i][j] = DT[i-1][j-1] % 1'000'000'000;
+                }
+                else{
+                    DT[i][j] = (DT[i-1][j-1] + DT[i-1][j+1]) % 1'000'000'000;
+                }
+            }
+        }
     }
 
-    return DT[n];
+    for(int i = 0; i < 10; i++){
+        ans += DT[n][i];
+    }
+
+    return ans % 1'000'000'000;
 }
 
 int main(){
-    int TC;
     int n;
-    std::cin >> TC;
-    for(int i = 0; i < TC; i++){
-        std::cin >> n;
-        std::fill((long long*)DT, (long long*)DT+n+1, 0);
-        std::cout << solve(n) << "\n";
-    }
+    std::cin >> n;
+    std::fill((long long *)DT, (long long*)DT+(MAX*10), 0);
+    std::cout << solve(n) << std::endl;
     return 0;
 }

82d94f99d6808220f7fbb571d813c5b2c5870e5b
heesoon <chipmaker0304@gmail.com>
2021-12-31 16:47:42 +0900

    temperal merge

--- BeakJoon/Dynamic_Programming_1/12865/c++/submit.cpp
+++ BeakJoon/Dynamic_Programming_1/12865/c++/submit.cpp
@@ -1,58 +1,44 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <utility>
 
-std::vector<int> LLIS;
-std::vector<int> RLIS;
-std::vector<int> S;
+std::vector<std::pair<int,int>> DT;
 
-int solve(int n){
+int solve(int n, int k){
     int ans = 0;
-
+    int a, b;
     for(int i = 0; i < n; i++){
-        for(int j = 0; j < i; j++){
-            if(S[j] < S[i]){
-                RLIS[i] = std::max(RLIS[i], RLIS[j]+1);
+        std::cin >> a >> b;
+
+        std::vector<std::pair<int,int>> TDT;
+        for(const auto &p : DT){
+            int na = p.first + a;
+            int nb = p.second + b;
+            TDT.push_back(std::make_pair(na, nb));
+        }
+
+        DT.insert(DT.begin(), TDT.begin(), TDT.end());
+
+        DT.push_back(std::make_pair(a, b));
+    }
+
+    for(const auto &p : DT){
+        //std::cout << p.first << " , " << p.second << std::endl;
+        if(p.first <= k){
+            if(ans < p.second){
+                ans = p.second;
             }
         }
-    }
-
-    for(int i = n-1; i >= 0; i--){
-        for(int j = n-1; j > i; j--){
-            if(S[j] < S[i]){
-                LLIS[i] = std::max(LLIS[i], LLIS[j]+1);
-            }
-        }
-    }
-
-#if 0
-    for(int i = 0; i < n; i++){
-        std::cout << RLIS[i] << " ";
-    }
-    std::cout << std::endl;
-    for(int i = 0; i < n; i++){
-        std::cout << LLIS[i] << " ";
-    }
-    std::cout << std::endl;
-#endif
-
-    for(int i = 0; i < n; i++){
-        ans = std::max(ans, RLIS[i] + LLIS[i] - 1);
     }
 
     return ans;
 }
 
 int main(){
-    int n;
-    std::cin >> n;
-    LLIS.assign(n, 1);
-    RLIS.assign(n, 1);
-    S.assign(n, 0);
+    int n, k;
+    std::cin >> n >> k;
 
-    for(int i = 0; i < n; i++){
-        std::cin >> S[i];
-    }
-
-    std::cout << solve(n) << std::endl;
+    std::cout << solve(n, k) << std::endl;
+    return 0;
 }

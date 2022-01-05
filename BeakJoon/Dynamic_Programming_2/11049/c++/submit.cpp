8606921545f6df28c5f03443acbd6395fd3d33c3
heesoon <chipmaker0304@gmail.com>
2022-01-05 13:43:50 +0900

    complete code

--- BeakJoon/Dynamic_Programming_2/11049/c++/submit.cpp
+++ BeakJoon/Dynamic_Programming_2/11049/c++/submit.cpp
@@ -7,20 +7,33 @@ std::vector<std::vector<int>> DT;
 std::vector<std::vector<int>> MAT;
 
 int solve(int n){
-    for(int d = 1; d < n; d++){
-        for(int i = 0; i+d < n; i++){
-            for(int j = i+; j < n; j++){
-                DT[i][j] = std::max(DT[i][j], )
+    for(int i = 0; i < n; i++){
+        DT[i][i] = 0;
+    }
+
+    for(int i = 1; i < n; i++){
+        for(int j = 0; j+i < n; j++){
+            for(int k = j; k < j+i; k++){
+                DT[j][j+i] = std::min(DT[j][j+i], DT[j][k] + DT[k+1][j+i] + MAT[j][0]*MAT[k][1]*MAT[i+j][1]);
             }
         }
     }
+#if 0
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            std::cout << DT[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+#endif
+    return DT[0][n-1];
 }
 
 int main(){
     int n;
 
     std::cin >> n;
-    DT.assign(n, std::vector<int>(n, 0));
+    DT.assign(n, std::vector<int>(n, std::numeric_limits<int>::max()/2));
     MAT.assign(n, std::vector<int>(2, 0));
 
     for(int i = 0; i < n; i++){

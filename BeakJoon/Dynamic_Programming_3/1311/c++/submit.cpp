da1b5abe6a2cc05e90059036d89f25d2e67312c1
heesoon <chipmaker0304@gmail.com>
2022-01-14 11:28:51 +0900

    update

--- BeakJoon/Dynamic_Programming_3/1311/c++/submit.cpp
+++ BeakJoon/Dynamic_Programming_3/1311/c++/submit.cpp
@@ -3,40 +3,27 @@
 #include <algorithm>
 #include <limits>
 
-std::vector<std::vector<int>> DT;
-std::vector<int> pSum;
-std::vector<int> novelCost;
+std::vector<std::vector<int>> works;
 
-int solve(int k){
-    for(int i = 1; i <= k; i++){
-        std::cin >> novelCost[i];
-        pSum[i] = pSum[i-1] + novelCost[i];
-    }
+int solve(int n){
 
-    for(int d = 1; d < k; d++){
-        for(int i = 1; i+d <= k; i++){
-            int j = i+d;
-            DT[i][j] = std::numeric_limits<int>::max();
-            for(int m = i; m < j; m++){
-                DT[i][j] = std::min(DT[i][j], DT[i][m] + DT[m+1][j] + pSum[j] - pSum[i-1]);
-            }
+}
+
+// perfect matching problem.
+int main(){
+    std::cin.tie(nullptr); std::cout.tie(nullptr);
+    std::ios_base::sync_with_stdio(false);
+
+    int n;
+    std::cin >> n;
+
+    works.assign(n, std::vector<int>(n, 0));
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            std::cin >> works[i][j];
         }
     }
 
-    return DT[1][k];
-}
-
-int main(){
-    int c, k;
-    std::cin >> c;
-    for(int i = 0; i < c; i++){
-        std::cin >> k;
-        DT.assign(k+1, std::vector<int>(k+1, 0));
-        pSum.assign(k+1, 0);
-        novelCost.assign(k+1, 0);
-        std::cout << solve(k) << std::endl;
-    }
+    std::cout << solve(n) << "\n";
     return 0;
 }
-
-//https://js1jj2sk3.tistory.com/3

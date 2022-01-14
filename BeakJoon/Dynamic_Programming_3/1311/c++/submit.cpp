128260e9a2a416daf404f53b1f3a3a0d155770d4
heesoon <chipmaker0304@gmail.com>
2022-01-14 17:08:21 +0900

    upate

--- BeakJoon/Dynamic_Programming_3/1311/c++/submit.cpp
+++ BeakJoon/Dynamic_Programming_3/1311/c++/submit.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <vector>
+#include <bitset>
 #include <algorithm>
 #include <limits>
 
 std::vector<std::vector<int>> works;
+std::vector<int> DT;
 
-int solve(int n){
+int solve(int s, int n){
+    int ret = 0;
 
+    ret = 
+
+    return ret;
 }
 
 // perfect matching problem.
@@ -18,12 +24,14 @@ int main(){
     std::cin >> n;
 
     works.assign(n, std::vector<int>(n, 0));
+    DT.assign(2 << n, -1);
+
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
             std::cin >> works[i][j];
         }
     }
 
-    std::cout << solve(n) << "\n";
+    std::cout << solve(0, n) << "\n";
     return 0;
 }

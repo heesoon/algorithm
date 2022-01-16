9c7c0ca3fb7d18d4821200f4e4a539bee6a17609
heesoon <chipmaker0304@gmail.com>
2022-01-16 12:51:22 +0900

    complete code but perfectly don't understand

--- BeakJoon/Dynamic_Programming_3/1311/c++/submit.cpp
+++ BeakJoon/Dynamic_Programming_3/1311/c++/submit.cpp
@@ -5,12 +5,26 @@
 #include <limits>
 
 std::vector<std::vector<int>> works;
-std::vector<int> DT;
+std::vector<std::vector<int>> DT;
+int n;
 
-int solve(int s, int n){
-    int ret = 0;
+int solve(int idx, int visited){
+    if(visited == (1 << n) - 1){
+        return 0;
+    }
 
-    ret = 
+    int &ret = DT[idx][visited];
+    if(ret != -1){
+        return ret;
+    }
+
+    ret = std::numeric_limits<int>::max()/2;
+
+    for(int i = 0; i < n; i++){
+        if(visited & (1 << i)) continue;
+
+        ret = std::min(ret, solve(idx+1, visited | (1 << i)) + works[idx][i]);
+    }
 
     return ret;
 }
@@ -19,12 +33,10 @@ int solve(int s, int n){
 int main(){
     std::cin.tie(nullptr); std::cout.tie(nullptr);
     std::ios_base::sync_with_stdio(false);
-
-    int n;
     std::cin >> n;
 
     works.assign(n, std::vector<int>(n, 0));
-    DT.assign(2 << n, -1);
+    DT.assign(21, std::vector<int>(2 << n, -1));
 
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
@@ -32,6 +44,8 @@ int main(){
         }
     }
 
-    std::cout << solve(0, n) << "\n";
+    std::cout << solve(0, 0) << "\n";
     return 0;
 }
+
+//https://technicolour.tistory.com/14

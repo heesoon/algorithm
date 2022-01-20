05b7a11b913ea845212a52fe68a785812ddae61f
heesoon <chipmaker0304@gmail.com>
2022-01-20 16:52:37 +0900

    complete code

--- BeakJoon/Dynamic_Programming_3/2482/c++/submit.cpp
+++ BeakJoon/Dynamic_Programming_3/2482/c++/submit.cpp
@@ -3,40 +3,29 @@
 #include <algorithm>
 #include <limits>
 
+const int M = 1'000'000'003;
 std::vector<std::vector<int>> DT;
-std::vector<int> pSum;
-std::vector<int> novelCost;
 
-int solve(int k){
-    for(int i = 1; i <= k; i++){
-        std::cin >> novelCost[i];
-        pSum[i] = pSum[i-1] + novelCost[i];
+int solve(int n, int k){
+    int &ret = DT[n][k];
+    if(n <= 0 || n < 2*k){
+        return ret = 0;
+    }
+    if(k == 1){
+        return ret = n;
     }
 
-    for(int d = 1; d < k; d++){
-        for(int i = 1; i+d <= k; i++){
-            int j = i+d;
-            DT[i][j] = std::numeric_limits<int>::max();
-            for(int m = i; m < j; m++){
-                DT[i][j] = std::min(DT[i][j], DT[i][m] + DT[m+1][j] + pSum[j] - pSum[i-1]);
-            }
-        }
-    }
+    if(ret != -1) return ret;
 
-    return DT[1][k];
+    return ret = (solve(n-2, k-1) + solve(n-1, k))%M;
 }
 
 int main(){
-    int c, k;
-    std::cin >> c;
-    for(int i = 0; i < c; i++){
-        std::cin >> k;
-        DT.assign(k+1, std::vector<int>(k+1, 0));
-        pSum.assign(k+1, 0);
-        novelCost.assign(k+1, 0);
-        std::cout << solve(k) << std::endl;
-    }
+    std::cin.tie(nullptr); std::cout.tie(nullptr); std::ios_base::sync_with_stdio(false);
+    int N, K;
+    std::cin >> N;
+    std::cin >> K;
+    DT.assign(N+1, std::vector<int>(N+1, -1));
+    std::cout << solve(N, K) << "\n";
     return 0;
 }
-
-//https://js1jj2sk3.tistory.com/3

82d4bbd4571afecfb3a155d5944b5b3d45bf49ac
heesoon <chipmaker0304@gmail.com>
2022-01-21 22:06:13 +0900

    complete code

--- BeakJoon/Backtracking/15650/c++/submit.cpp
+++ BeakJoon/Backtracking/15650/c++/submit.cpp
@@ -6,8 +6,8 @@
 int N, M;
 std::vector<int> v(9, 0);
 
-void solve(int idx, int visited){
-    if(idx == M){
+void solve(int idx, int cnt, int visited){
+    if(cnt == M){
         for(int i = 0; i < M; i++){
             std::cout << v[i] << " ";
         }
@@ -15,16 +15,16 @@ void solve(int idx, int visited){
         return;
     }
 
-    for(int i = 1; i <= N; i++){
+    for(int i = idx+1; i <= N; i++){
         if(visited & (1 << i)) continue;
-        v[idx] = i;
-        solve(idx+1, visited | (1 << i));
+        v[cnt] = i;
+        solve(i, cnt+1, visited | (1 << i));
     }
 }
 
 int main(){
     std::cin.tie(nullptr); std::cout.tie(nullptr); std::ios_base::sync_with_stdio(false);
     std::cin >> N >> M;
-    solve(0, 0);
+    solve(0, 0, 0);
     return 0;
 }

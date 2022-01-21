2d0df0c979e5244805a1678d5e3247de30374553
heesoon <chipmaker0304@gmail.com>
2022-01-21 16:04:44 +0900

    complete code

--- BeakJoon/Backtracking/15649/c++/submit.cpp
+++ BeakJoon/Backtracking/15649/c++/submit.cpp
@@ -3,37 +3,30 @@
 #include <algorithm>
 #include <utility>
 
-const int WMAX = 100'001;
-std::vector<std::vector<int>> DT;
-std::vector<int> W;
-std::vector<int> V;
+int N, M;
 
-int solve(int n, int k){
-    int ans = 0;
-    for(int i = 1; i <= n; i++){
-        std::cin >> W[i] >> V[i];
-    }
-
-    for(int i = 1; i <= n; i++){
-        for(int j = 0; j <= k; j++){
-            if(j-W[i] >= 0) DT[i][j] = std::max(DT[i-1][j], DT[i-1][j-W[i]]+V[i]);
-            else DT[i][j] = DT[i-1][j];
+void solve(int visited, int m, std::vector<int> v){
+    if(m == M){
+        for(int i = 0; i < m; i++){
+            std::cout << v[i] << " ";
         }
+        std::cout << "\n";
+        return;
     }
 
-    for(int j = 0; j <= k; j++){
-        ans = std::max(ans, DT[n][j]);
+    for(int i = 1; i <= N; i++){
+        if(visited & (1 << i)) continue;
+        v.push_back(i);
+        solve(visited|(1 << i), m+1, v);
+        v.pop_back();
     }
-
-    return ans;
 }
 
 int main(){
-    int n, k;
-    std::cin >> n >> k;
-    DT.assign(n+1, std::vector<int>(WMAX, 0));
-    W.assign(n+1, 0);
-    V.assign(n+1, 0);
-    std::cout << solve(n, k) << std::endl;
+    std::cin.tie(nullptr); std::cout.tie(nullptr); std::ios_base::sync_with_stdio(false);
+    std::cin >> N >> M;
+    int visited = 0;
+    std::vector<int> v;
+    solve(visited, 0, v);
     return 0;
 }

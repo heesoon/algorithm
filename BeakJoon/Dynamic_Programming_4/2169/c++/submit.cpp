f308c507555272ee615050ce2baa90c9100f53e6
heesoon <chipmaker0304@gmail.com>
2022-01-25 11:16:20 +0900

    update

--- BeakJoon/Dynamic_Programming_4/2169/c++/submit.cpp
+++ BeakJoon/Dynamic_Programming_4/2169/c++/submit.cpp
@@ -3,40 +3,55 @@
 #include <algorithm>
 #include <limits>
 
-std::vector<std::vector<int>> DT;
-std::vector<int> pSum;
-std::vector<int> novelCost;
+const int L = 1'001;
+int N, M;
+int dy[3] = {0, 1, 0};
+int dx[3] = {1, 0, -1};
 
-int solve(int k){
-    for(int i = 1; i <= k; i++){
-        std::cin >> novelCost[i];
-        pSum[i] = pSum[i-1] + novelCost[i];
+std::vector<std::vector<int>> DT;
+std::vector<std::vector<int>> map;
+std::vector<std::vector<bool>> visited;
+
+bool in_range(int y, int x){
+    if((y >= 0 && y < N) && (x >= 0 && x < M)){
+        return true;
+    }
+    return false;
+}
+
+int solve(int y, int x){
+    int &ret = DT[y][x];
+    if(ret != -1) return ret;
+
+    if(y == N-1 && x == M-1){
+        return ret = map[y][x];
     }
 
-    for(int d = 1; d < k; d++){
-        for(int i = 1; i+d <= k; i++){
-            int j = i+d;
-            DT[i][j] = std::numeric_limits<int>::max();
-            for(int m = i; m < j; m++){
-                DT[i][j] = std::min(DT[i][j], DT[i][m] + DT[m+1][j] + pSum[j] - pSum[i-1]);
-            }
+    ret = 0;
+    for(int i = 0; i < 3; i++){
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if(visited[ny][nx] == false && in_range(ny, nx) == true){
+            ret = std::max(ret, solve(ny, nx) + map[y][x]);
         }
     }
 
-    return DT[1][k];
+    return ret;
 }
 
 int main(){
-    int c, k;
-    std::cin >> c;
-    for(int i = 0; i < c; i++){
-        std::cin >> k;
-        DT.assign(k+1, std::vector<int>(k+1, 0));
-        pSum.assign(k+1, 0);
-        novelCost.assign(k+1, 0);
-        std::cout << solve(k) << std::endl;
+    std::cin.tie(nullptr); std::cout.tie(nullptr); std::ios_base::sync_with_stdio(false);
+    DT.assign(L, std::vector<int>(L, -1));
+    map.assign(L, std::vector<int>(L, 0));
+    visited.assign(L, std::vector<bool>(L, false));
+
+    std::cin >> N >> M;
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            std::cin >> map[i][j];
+        }
     }
+
+    std::cout << solve(0, 0) << "\n";
     return 0;
 }
-
-//https://js1jj2sk3.tistory.com/3

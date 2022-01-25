a112a31cc1535ac2c430b96f3a97b924d4be584c
heesoon <chipmaker0304@gmail.com>
2022-01-25 13:22:45 +0900

    complete code

--- BeakJoon/Dynamic_Programming_4/2169/c++/submit.cpp
+++ BeakJoon/Dynamic_Programming_4/2169/c++/submit.cpp
@@ -5,12 +5,10 @@
 
 const int L = 1'001;
 int N, M;
-int dy[3] = {0, 1, 0};
-int dx[3] = {1, 0, -1};
+int dx[3] = {1, 0, -1}, dy[3] = {0, 1, 0};
 
-std::vector<std::vector<int>> DT;
+std::vector<std::vector<std::vector<int>>> DT;
 std::vector<std::vector<int>> map;
-std::vector<std::vector<bool>> visited;
 
 bool in_range(int y, int x){
     if((y >= 0 && y < N) && (x >= 0 && x < M)){
@@ -19,21 +17,22 @@ bool in_range(int y, int x){
     return false;
 }
 
-int solve(int y, int x){
-    int &ret = DT[y][x];
+int solve(int y, int x, int d){
+    int &ret = DT[y][x][d];
     if(ret != -1) return ret;
 
     if(y == N-1 && x == M-1){
         return ret = map[y][x];
     }
 
-    ret = 0;
+    ret = std::numeric_limits<int>::min()/2;
     for(int i = 0; i < 3; i++){
         int ny = y + dy[i];
         int nx = x + dx[i];
-        if(visited[ny][nx] == false && in_range(ny, nx) == true){
-            ret = std::max(ret, solve(ny, nx) + map[y][x]);
-        }
+
+        if((d+2)%4 == i) continue;
+        if(in_range(ny, nx) == false) continue;
+        ret = std::max(ret, solve(ny, nx, i) + map[y][x]);
     }
 
     return ret;
@@ -41,9 +40,8 @@ int solve(int y, int x){
 
 int main(){
     std::cin.tie(nullptr); std::cout.tie(nullptr); std::ios_base::sync_with_stdio(false);
-    DT.assign(L, std::vector<int>(L, -1));
+    DT.assign(L, std::vector<std::vector<int>>(L, std::vector<int>(3, -1)));
     map.assign(L, std::vector<int>(L, 0));
-    visited.assign(L, std::vector<bool>(L, false));
 
     std::cin >> N >> M;
     for(int i = 0; i < N; i++){
@@ -52,6 +50,8 @@ int main(){
         }
     }
 
-    std::cout << solve(0, 0) << "\n";
+    std::cout << solve(0, 0, 1) << "\n";
     return 0;
 }
+
+https://degurii.tistory.com/182

733fea76c93fdfb682a86dab45b8f86b2d0998b5
heesoon <chipmaker0304@gmail.com>
2022-01-28 18:58:56 +0900

    update

--- BeakJoon/Backtracking/2580/c++/submit.cpp
+++ BeakJoon/Backtracking/2580/c++/submit.cpp
@@ -3,34 +3,14 @@
 #include <algorithm>
 #include <utility>
 
-const int N = 16;
-std::vector<bool> bColum(N, false);
-std::vector<bool> bUpDiagonal(2*N, false);
-std::vector<bool> bDownDiagonal(2*N, false);
-
 int solve(int y, int n){
-    int ret = 0;
-    if(y == n-1){
-        //std::cout << y << std::endl;
-        return 1;
-    }
 
-    for(int nx = 0; nx < n; nx++){
-        int ny = y+1;
-        if(bColum[nx] == false && bUpDiagonal[nx+ny] == false && bDownDiagonal[n+(nx - ny)] == false){
-            bColum[nx] = bUpDiagonal[nx+ny] = bDownDiagonal[n+(nx - ny)] = true;
-            ret += solve(ny, n);
-            bColum[nx] = bUpDiagonal[nx+ny] = bDownDiagonal[n+(nx - ny)] = false;
-        }
-    }
-
-    return ret;
 }
 
 int main(){
     std::cin.tie(nullptr); std::cout.tie(nullptr); std::ios_base::sync_with_stdio(false);
-    int n;
-    std::cin >> n;
-    std::cout << solve(-1, n) << "\n";
+
     return 0;
 }
+
+// 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

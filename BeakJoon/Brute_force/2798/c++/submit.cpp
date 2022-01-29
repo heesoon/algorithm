447d7b06544c0f08c1c9f2dfb449642764ef70d3
heesoon <chipmaker0304@gmail.com>
2022-01-30 03:48:25 +0900

    complete code

--- BeakJoon/Brute_force/2798/c++/submit.cpp
+++ BeakJoon/Brute_force/2798/c++/submit.cpp
@@ -2,61 +2,43 @@
 #include <vector>
 #include <algorithm>
 #include <utility>
+#include <limits>
 
-const int N = 9;
-std::vector<std::vector<int>> map;
-std::vector<std::vector<bool>> col;
-std::vector<std::vector<bool>> row;
-std::vector<std::vector<bool>> box3x3;
+const int N = 101;
+std::vector<int> cards;
+//std::vector<bool> visited;
+//int ans = std::numeric_limits<int>::max()/2;
+int ans = 0;
 
-void solve(int cnt){
-    if(cnt == 81){
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < N; j++){
-                std::cout << map[i][j] << " ";
-            }
-            std::cout << "\n";
+void solve(int cnt, int idx, int sum, int m, int n){
+    if(cnt == 3){
+        if(sum <= m && sum > ans){
+            ans = sum;
         }
-        exit(0);
+        return;
     }
 
-    int y = cnt/N;
-    int x = cnt%N;
-
-    if(map[y][x]){
-        solve(cnt+1);
-    }
-    else{
-        for(int i = 1; i <= N; i++){
-            if(col[x][i] == false && row[y][i] == false && box3x3[(y/3)*3 + x/3][i] == false){
-                map[y][x] = i;
-                col[x][i] = row[y][i] = box3x3[(y/3)*3 + x/3][i] = true;
-                solve(cnt+1);
-                map[y][x] = 0;
-                col[x][i] = row[y][i] = box3x3[(y/3)*3 + x/3][i] = false;
-            }
-        }
+    for(int i = idx; i < n; i++){
+        //if(visited[i] == false){
+        //    visited[i] = true;
+            solve(cnt+1, i+1, sum+cards[i], m, n);
+        //    visited[i] = false;
+        //}
     }
 }
 
 int main(){
     std::cin.tie(nullptr); std::cout.tie(nullptr); std::ios_base::sync_with_stdio(false);
-    map.assign(N, std::vector<int>(N, 0));
-    col.assign(N+1, std::vector<bool>(N+1, false));
-    row.assign(N+1, std::vector<bool>(N+1, false));
-    box3x3.assign(N, std::vector<bool>(N+1, false));
-
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
-            std::cin >> map[i][j];
-            if(map[i][j]){
-                row[i][map[i][j]] = true;
-                col[j][map[i][j]] = true;
-                box3x3[(i/3)*3 + j/3][map[i][j]] = true;
-            }
-        }
+    int n, m;
+    cards.assign(N, 0);
+//    visited.assign(N, false);
+    std::cin >> n >> m;
+    for(int i = 0; i < n; i++){
+        std::cin >> cards[i];
     }
 
-    solve(0);
+    solve(0, 0, 0, m, n);
+
+    std::cout << ans << "\n";
     return 0;
 }

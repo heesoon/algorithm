578d5fa69fd33ebeb872aadda15da375c35b79bb
heesoon <chipmaker0304@gmail.com>
2022-01-29 16:02:16 +0900

    complete code

--- BeakJoon/Backtracking/14888/c++/submit.cpp
+++ BeakJoon/Backtracking/14888/c++/submit.cpp
@@ -2,61 +2,66 @@
 #include <vector>
 #include <algorithm>
 #include <utility>
+#include <limits>
 
-const int N = 9;
-std::vector<std::vector<int>> map;
-std::vector<std::vector<bool>> col;
-std::vector<std::vector<bool>> row;
-std::vector<std::vector<bool>> box3x3;
+const int N = 12;
+int minValue = std::numeric_limits<int>::max()/2;
+int maxValue = std::numeric_limits<int>::min()/2;
+std::vector<int> nums;
+std::vector<int> operators;
 
-void solve(int cnt){
-    if(cnt == 81){
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < N; j++){
-                std::cout << map[i][j] << " ";
-            }
-            std::cout << "\n";
+void solve(int idx, int n, int sum){
+    if(idx == n){
+        if(sum > maxValue){
+            maxValue = sum;
         }
-        exit(0);
+        
+        if(sum < minValue){
+            minValue = sum;
+        }
+
+        return;
     }
 
-    int y = cnt/N;
-    int x = cnt%N;
-
-    if(map[y][x]){
-        solve(cnt+1);
-    }
-    else{
-        for(int i = 1; i <= N; i++){
-            if(col[x][i] == false && row[y][i] == false && box3x3[(y/3)*3 + x/3][i] == false){
-                map[y][x] = i;
-                col[x][i] = row[y][i] = box3x3[(y/3)*3 + x/3][i] = true;
-                solve(cnt+1);
-                map[y][x] = 0;
-                col[x][i] = row[y][i] = box3x3[(y/3)*3 + x/3][i] = false;
+    for(int i = 0; i < 4; i++){
+        if(operators[i]){
+            operators[i] -= 1;
+            if(i == 0){
+                solve(idx+1, n, sum+nums[idx]);
             }
+            else if(i == 1){
+                solve(idx+1, n, sum-nums[idx]);
+            }
+            else if(i == 2){
+                solve(idx+1, n, sum*nums[idx]);
+            }
+            else{
+                solve(idx+1, n, sum/nums[idx]);
+            }            
+            operators[i] += 1;
         }
     }
 }
 
 int main(){
     std::cin.tie(nullptr); std::cout.tie(nullptr); std::ios_base::sync_with_stdio(false);
-    map.assign(N, std::vector<int>(N, 0));
-    col.assign(N+1, std::vector<bool>(N+1, false));
-    row.assign(N+1, std::vector<bool>(N+1, false));
-    box3x3.assign(N, std::vector<bool>(N+1, false));
+    int n;
+    nums.assign(N, 0);
+    operators.assign(4, 0);
 
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
-            std::cin >> map[i][j];
-            if(map[i][j]){
-                row[i][map[i][j]] = true;
-                col[j][map[i][j]] = true;
-                box3x3[(i/3)*3 + j/3][map[i][j]] = true;
-            }
-        }
+    std::cin >> n;
+    for(int i = 0; i < n; i++){
+        std::cin >> nums[i];
     }
 
-    solve(0);
+    for(int i = 0; i < 4; i++){
+        std::cin >> operators[i];
+    }
+
+    solve(1, n, nums[0]);
+
+    std::cout << maxValue << "\n";
+    std::cout << minValue << "\n";
+
     return 0;
 }

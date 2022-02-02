f020b247ccbb1498a91ad38dac6563c8bc1e6841
unknown <chipmaker0304@gmail.com>
2022-02-02 16:51:00 +0900

    complete code

--- BeakJoon/Greedy/1541/c++/submit.cpp
+++ BeakJoon/Greedy/1541/c++/submit.cpp
@@ -4,12 +4,34 @@
 #include <utility>
 #include <limits>
 
-void solve(int cnt){
-
-}
-
 int main(){
     std::cin.tie(nullptr); std::cout.tie(nullptr); std::ios_base::sync_with_stdio(false);
+    std::string str;
+    int num = 0, sum = 0;
+    bool bMinus = false;
+    std::cin >> str;
+    for(int i = 0; i <= str.size(); i++){
+        if(str[i] == '+' || str[i] == '-' || i == str.size()){
 
+            if(bMinus == false){
+                sum += num;
+                num = 0;
+            }
+            else{
+                sum -= num;
+                num = 0;
+            }
+            
+            if(str[i] == '-'){
+                bMinus = true;
+            }            
+        }
+        else{
+            num *= 10;
+            num += str[i] - '0';
+        }
+    }
+
+    std::cout << sum << "\n";
     return 0;
 }

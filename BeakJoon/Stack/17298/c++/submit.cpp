7a72fb09a29f4933ce432a8c5a40654d4dd84779
unknown <chipmaker0304@gmail.com>
2022-02-03 21:14:53 +0900

    complete code

--- BeakJoon/Stack/17298/c++/submit.cpp
+++ BeakJoon/Stack/17298/c++/submit.cpp
@@ -11,7 +11,7 @@ int main(){
     std::cin >> n;
     std::stack<int> st;
     std::vector<int> v(n);
-    std::vector<int> ans;
+    std::vector<int> ans(n);
     
     for(int i = 0; i < n; i++){
         std::cin >> v[i];
@@ -19,11 +19,17 @@ int main(){
 
     for(int i = 0; i < n; i++){
         while(!st.empty() && v[st.top()] < v[i]){
-            ans.push_back(v[st.top()]);
+            ans[st.top()] = v[i];
             st.pop();
         }
 
         st.push(i);
+    }
+
+    while(!st.empty()){
+        int idx = st.top();
+        st.pop();
+        ans[idx] = -1;
     }
 
     for(int i = 0; i < ans.size(); i++){

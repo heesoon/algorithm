c513f862541e4d5203dc543c5e651cf5e09721d9
unknown <chipmaker0304@gmail.com>
2022-02-03 13:13:30 +0900

    complete code

--- BeakJoon/Stack/10773/c++/submit.cpp
+++ BeakJoon/Stack/10773/c++/submit.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 #include <utility>
 #include <limits>
+#include <stack>
 
 int main(){
     std::cin.tie(nullptr); std::cout.tie(nullptr); std::ios_base::sync_with_stdio(false);
+    int k, n;
+    std::cin >> k;
+    std::stack<int> st;
+    for(int i = 0; i < k; i++){
+        std::cin >> n;
+        if(n == 0){
+            st.pop();
+        }
+        else{
+            st.push(n);
+        }
+    }
 
+    int sum = 0;
+    while(!st.empty()){
+        int t = st.top();
+        st.pop();
+        sum += t;
+    }
+
+    std::cout << sum << "\n";
     return 0;
 }

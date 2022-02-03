09f02a2b2250e6d68e2b879f8f5cfcddd42cb3f1
unknown <chipmaker0304@gmail.com>
2022-02-03 12:14:29 +0900

    complete code

--- BeakJoon/Baisc_Mathematics_1/1712/c++/submit.cpp
+++ BeakJoon/Baisc_Mathematics_1/1712/c++/submit.cpp
@@ -7,6 +7,13 @@ int main(){
     std::cin.tie(nullptr); std::cout.tie(nullptr); std::ios_base::sync_with_stdio(false);
     int A, B, C;
     std::cin >> A >> B >> C;
-    
+
+    if(B >= C){
+        std::cout << -1 << "\n";
+    }
+    else{
+        std::cout << A/(C-B)+1 << "\n";
+    }
+
     return 0;
 }

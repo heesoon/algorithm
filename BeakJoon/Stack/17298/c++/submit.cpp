9d9a1f86ac0f55ac68dfb08a0a77ef52c426479c
unknown <chipmaker0304@gmail.com>
2022-02-03 21:08:14 +0900

    temporal

--- BeakJoon/Stack/17298/c++/submit.cpp
+++ BeakJoon/Stack/17298/c++/submit.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 #include <algorithm>
 #include <utility>
 #include <limits>
 
 int main(){
     std::cin.tie(nullptr); std::cout.tie(nullptr); std::ios_base::sync_with_stdio(false);
+    int n;
+    std::cin >> n;
+    std::stack<int> st;
+    std::vector<int> v(n);
+    std::vector<int> ans;
+    
+    for(int i = 0; i < n; i++){
+        std::cin >> v[i];
+    }
 
+    for(int i = 0; i < n; i++){
+        while(!st.empty() && v[st.top()] < v[i]){
+            ans.push_back(v[st.top()]);
+            st.pop();
+        }
+
+        st.push(i);
+    }
+
+    for(int i = 0; i < ans.size(); i++){
+        std::cout << ans[i] << " ";
+    }
+    std::cout << "\n";
     return 0;
 }
+
+// https://suhwanc.tistory.com/58

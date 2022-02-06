d031df5783e63f393e59fa9a3a7a983e284598d6
unknown <chipmaker0304@gmail.com>
2022-02-06 21:05:20 +0900

    complete code

--- BeakJoon/Divider_conquer/11401/c++/submit.cpp
+++ BeakJoon/Divider_conquer/11401/c++/submit.cpp
@@ -4,29 +4,41 @@
 #include <utility>
 #include <limits>
 
-long long solve(long long a, long long b, long long c){
-    long long ans = 0;
-    if(b == 1){
-        return a%c;
-    }
-    else{
-        long long t = solve(a, b/2, c);
-        if(b%2 == 0){
-            ans = (t*t)%c;
-        }
-        else{
-            ans = ((t*t)%c*a)%c;
-        }
+const long long DIV = 1'000'000'007;
+
+long long factorial(long long n){
+    long long ans = 1;
+    for(long long i = 1; i <= n; i++){
+        ans *= i;
+        ans %= DIV;
     }
 
     return ans;
 }
 
+long long cpower(long long base, long long power){
+    if(power == 1){
+        return base%DIV;
+    }
+    else{
+        long long t = cpower(base, power/2);
+        if(power%2 == 0){
+            return (t*t)%DIV;
+        }
+        else{
+            return ((t*t)%DIV*base)%DIV;
+        }
+    }
+}
+
 int main(){
     std::cin.tie(nullptr); std::cout.tie(nullptr); std::ios_base::sync_with_stdio(false);
-    long long a, b, c;
-    std::cin >> a >> b >> c;
+    long long n, k;
+    std::cin >> n >> k;
+    long long numerator = factorial(n);
+    long long denominator = (factorial(k)*factorial(n-k))%DIV;
+    denominator = cpower(denominator, DIV - 2);
 
-    std::cout << solve(a, b, c) << "\n";
+    std::cout << (numerator*denominator)%DIV << "\n";
     return 0;
 }

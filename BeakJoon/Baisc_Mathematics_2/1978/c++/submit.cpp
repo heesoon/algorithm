61dcc724960f006c7f135aacfca31bf5ef269c04
heesoon <chipmaker0304@gmail.com>
2022-02-08 11:08:34 +0900

    complete code

--- BeakJoon/Baisc_Mathematics_2/1978/c++/submit.cpp
+++ BeakJoon/Baisc_Mathematics_2/1978/c++/submit.cpp
@@ -3,17 +3,36 @@
 #include <algorithm>
 #include <limits>
 
+bool solve(int n){
+    if(n <= 1){
+        return false;
+    }
+
+    for(int i = 2; i*i <= n; i++){
+        if(n%i == 0){
+            return false;
+        }
+    }
+
+    return true;
+}
+
 int main(){
     std::cin.tie(nullptr); std::cout.tie(nullptr); std::ios_base::sync_with_stdio(false);
-    int A, B, C;
-    std::cin >> A >> B >> C;
+    int n, cnt = 0;
+    std::cin >> n;
+    std::vector<int> v(n);
 
-    if(B >= C){
-        std::cout << -1 << "\n";
-    }
-    else{
-        std::cout << A/(C-B)+1 << "\n";
+    for(int i = 0; i < n; i++){
+        std::cin >> v[i];
     }
 
+    for(int i = 0; i < n; i++){
+        if(solve(v[i]) == true){
+            cnt++;
+        }
+    }
+
+    std::cout << cnt << "\n";
     return 0;
 }

a0c8a793f8db4a66ffdd8f59bf0234ef7f525c62
heesoon <chipmaker0304@gmail.com>
2022-02-08 11:37:27 +0900

    complete code

--- BeakJoon/Baisc_Mathematics_2/11653/c++/submit.cpp
+++ BeakJoon/Baisc_Mathematics_2/11653/c++/submit.cpp
@@ -2,18 +2,23 @@
 #include <vector>
 #include <algorithm>
 #include <limits>
+#include <cmath>
 
 int main(){
     std::cin.tie(nullptr); std::cout.tie(nullptr); std::ios_base::sync_with_stdio(false);
-    int A, B, C;
-    std::cin >> A >> B >> C;
+    int n;
+    std::cin >> n;
 
-    if(B >= C){
-        std::cout << -1 << "\n";
-    }
-    else{
-        std::cout << A/(C-B)+1 << "\n";
+//    for(int i = 2; i <= n; i++){
+    for(int i = 2; i <= sqrt(n); i++){
+        while(n%i == 0){
+            std::cout << i << "\n";
+            n /= i;
+        }
     }
 
+    if(n != 1){
+        std::cout << n << "\n";
+    }
     return 0;
 }

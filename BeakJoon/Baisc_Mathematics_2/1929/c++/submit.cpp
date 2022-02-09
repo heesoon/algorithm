b50c5c572fac6b7075b48fcf491a9bcbbf44b474
heesoon <chipmaker0304@gmail.com>
2022-02-09 20:41:17 +0900

    complete code

--- BeakJoon/Baisc_Mathematics_2/1929/c++/submit.cpp
+++ BeakJoon/Baisc_Mathematics_2/1929/c++/submit.cpp
@@ -2,19 +2,31 @@
 #include <vector>
 #include <algorithm>
 #include <limits>
+#include <cmath>
 
 int main(){
     std::cin.tie(nullptr); std::cout.tie(nullptr); std::ios_base::sync_with_stdio(false);
     int n, m;
-    std::cin >> n >> m;
-    std::vector<int> v(n+1);
+    std::cin >> m >> n;
+    std::vector<int> v(n+1, 0);
 
-    for(int i = 0; i <= n; i++){
+    for(int i = 2; i <= n; i++){
         v[i] = i;
     }
 
-    for(int i = 2 i <= n; i++){
-        for(int j = i*2; j <= n; j += i)
+    for(int i = 2; i <= std::sqrt(n); i++){
+    //for(int i = 2; i <= n; i++){
+        if(v[i] == 0) continue;
+
+        for(int j = i*i; j <= n; j += i){
+            v[j] = 0;
+        }
+    }
+
+    for(int i = m; i <= n; i++){
+        if(v[i] != 0){
+            std::cout << v[i] << "\n";
+        }
     }
 
     return 0;

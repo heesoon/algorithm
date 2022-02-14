6b7634f202a8b9772de940e909a4a3ad4cc5818e
unknown <chipmaker0304@gmail.com>
2022-02-14 10:58:49 +0900

    complete code

--- BeakJoon/DFS_BFS/2606/c++/submit.cpp
+++ BeakJoon/DFS_BFS/2606/c++/submit.cpp
@@ -1,31 +1,45 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 #include <algorithm>
 #include <utility>
 #include <limits>
 
-int main(){
-    std::cin.tie(nullptr); std::cout.tie(nullptr); std::ios_base::sync_with_stdio(false);
-    long long n, k;
-    std::cin >> n >> k;
+int n, m;
+std::vector<int> G[101];
+std::vector<bool> visited(101, false);
+int solve(int node){
+    int cnt = 0;
+    std::queue<int> Q;
+    Q.push(node);
+    visited[node] = true;
 
-    long long lo = 1, hi = n*n, ans = 0;
-    while(lo <= hi){
-        long long mid = (lo+hi)/2;
-        long long cnt = 0;
-        for(long long i = 1; i <= n; i++){
-            cnt += std::min(n, mid/i);
-        }
-
-        if(cnt >= k){
-            ans = mid;
-            hi = mid-1;
-        }
-        else{
-            lo = mid+1;
+    while(!Q.empty()){
+        int fnode = Q.front();
+        Q.pop();
+        for(auto it = G[fnode].begin(); it != G[fnode].end(); it++){
+            int nextNode = *it;
+            if(visited[nextNode] == false){
+                visited[nextNode] = true;
+                cnt++;
+                Q.push(nextNode);
+            }
         }
     }
 
-    std::cout << ans << "\n";
+    return cnt;
+}
+
+int main(){
+    std::cin.tie(nullptr); std::cout.tie(nullptr); std::ios_base::sync_with_stdio(false);
+    std::cin >> n >> m;
+    for(int i = 0; i < m; i++){
+        int from, to;
+        std::cin >> from >> to;
+        G[from].push_back(to);
+        G[to].push_back(from);
+    }
+
+    std::cout << solve(1) << "\n";
     return 0;
 }

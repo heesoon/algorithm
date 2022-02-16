c3895e4da82def217ecb6004a97fe4d9d7fa6d41
heesoon <chipmaker0304@gmail.com>
2022-02-16 15:56:14 +0900

    update

--- BeakJoon/DFS_BFS/2206/c++/submit.cpp
+++ BeakJoon/DFS_BFS/2206/c++/submit.cpp
@@ -20,8 +20,8 @@ bool inRange(int y, int x){
 }
 
 int solve(){
-    int ans = numeric_limits<int>::max()/2;
-    std::queue<std::tuple<int,int,int>> Q;
+    int ans = std::numeric_limits<int>::max()/2;
+    std::queue<std::tuple<int,int,int,int>> Q;
     Q.push(std::make_tuple(0, 0, 0, 1));
 
     while(!Q.empty()){
@@ -29,6 +29,7 @@ int solve(){
         int fy = std::get<1>(Q.front());
         int fb = std::get<2>(Q.front()); // 벽을 부셨는지 아닌지
         int fd = std::get<3>(Q.front());
+        Q.pop();
 
         if(fx == m-1 && fy == n-1){
             ans = std::min(ans, fd);
@@ -41,23 +42,23 @@ int solve(){
             if(inRange(ny, nx) == true){
                 if(map[ny][nx] == 0 && visited[ny][nx][fb] == false){
                     visited[ny][nx][fb] = true;
-                    Q.push(ny, nx, fb, fd+1);
+                    Q.push(std::make_tuple(nx, ny, fb, fd+1));
                 }
-                if(map[ny][nx] == 1 && && fb == 0 && visited[ny][nx][1] == false){
+                if(map[ny][nx] == 1 && fb == 0 && visited[ny][nx][1] == false){
                     visited[ny][nx][1] = true;
-                    Q.push(ny, nx, 1, fd+1);
+                    Q.push(std::make_tuple(nx, ny, 1, fd+1));
                 }
             }
         }        
     }
 
-    return (ans == numeric_limits<int>::max()/2) ? -1 : ans;
+    return (ans == std::numeric_limits<int>::max()/2) ? -1 : ans;
 }
 
 int main(){
     std::cin.tie(nullptr); std::cout.tie(nullptr); std::ios_base::sync_with_stdio(false);
     std::cin >> n >> m;
-    map.assing(n, std::vector<int>(m, 0));
+    map.assign(n, std::vector<int>(m, 0));
     visited.assign(n, std::vector<std::vector<bool>>(m, std::vector<bool>(2, false)));
     for(int y = 0; y < n; y++){
         std::string str;
@@ -67,6 +68,6 @@ int main(){
         }
     }
 
-    std::cout < solve() << "\n";
+    std::cout << solve() << "\n";
     return 0;
 }

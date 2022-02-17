712160c453ded45e4bedd9f2ec851cf9e076532b
heesoon <chipmaker0304@gmail.com>
2022-02-17 12:54:52 +0900

    complete code

--- BeakJoon/Tree/11725/c++/submit.cpp
+++ BeakJoon/Tree/11725/c++/submit.cpp
@@ -2,61 +2,46 @@
 #include <vector>
 #include <algorithm>
 #include <utility>
+#include <limits>
 
-const int N = 9;
-std::vector<std::vector<int>> map;
-std::vector<std::vector<bool>> col;
-std::vector<std::vector<bool>> row;
-std::vector<std::vector<bool>> box3x3;
+int n;
+std::vector<int> parentStore;
+std::vector<std::vector<int>> tree;
+std::vector<bool> visited;
 
-void solve(int cnt){
-    if(cnt == 81){
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < N; j++){
-                std::cout << map[i][j] << " ";
-            }
-            std::cout << "\n";
-        }
-        exit(0);
+void printParents(){
+    for(int i = 2; i <= n; i++){
+        std::cout << parentStore[i] << "\n";
     }
+}
 
-    int y = cnt/N;
-    int x = cnt%N;
-
-    if(map[y][x]){
-        solve(cnt+1);
-    }
-    else{
-        for(int i = 1; i <= N; i++){
-            if(col[x][i] == false && row[y][i] == false && box3x3[(y/3)*3 + x/3][i] == false){
-                map[y][x] = i;
-                col[x][i] = row[y][i] = box3x3[(y/3)*3 + x/3][i] = true;
-                solve(cnt+1);
-                map[y][x] = 0;
-                col[x][i] = row[y][i] = box3x3[(y/3)*3 + x/3][i] = false;
-            }
+void solve(int sIdx){
+    for(auto it = tree[sIdx].begin(); it != tree[sIdx].end(); it++){
+        int nIdx = *it;
+        if(visited[nIdx] == false){
+            visited[nIdx] = true;
+            parentStore[nIdx] = sIdx;
+            solve(nIdx);
         }
     }
 }
 
 int main(){
     std::cin.tie(nullptr); std::cout.tie(nullptr); std::ios_base::sync_with_stdio(false);
-    map.assign(N, std::vector<int>(N, 0));
-    col.assign(N+1, std::vector<bool>(N+1, false));
-    row.assign(N+1, std::vector<bool>(N+1, false));
-    box3x3.assign(N, std::vector<bool>(N+1, false));
+    std::cin >> n;
+    tree.assign(n+1, std::vector<int>(0, 0));
+    parentStore.assign(n+1, 0);
+    visited.assign(n+1, false);
 
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
-            std::cin >> map[i][j];
-            if(map[i][j]){
-                row[i][map[i][j]] = true;
-                col[j][map[i][j]] = true;
-                box3x3[(i/3)*3 + j/3][map[i][j]] = true;
-            }
-        }
+    for(int i = 0; i < n-1; i++){
+        int f, t;
+        std::cin >> f >> t;
+        tree[f].push_back(t);
+        tree[t].push_back(f);
     }
 
-    solve(0);
+    visited[1] = true;
+    solve(1);
+    printParents();
     return 0;
 }

75073ac975e1927d8f97cea0c04e083f83914b78
unknown <chipmaker0304@gmail.com>
2022-02-18 17:54:19 +0900

    complete code

--- BeakJoon/Tree/1967/c++/submit.cpp
+++ BeakJoon/Tree/1967/c++/submit.cpp
@@ -1,62 +1,56 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 #include <algorithm>
 #include <utility>
+#include <limits>
 
-const int N = 9;
-std::vector<std::vector<int>> map;
-std::vector<std::vector<bool>> col;
-std::vector<std::vector<bool>> row;
-std::vector<std::vector<bool>> box3x3;
+int n;
+std::vector<bool> visited;
+std::vector<std::vector<std::pair<int,int>>> G;
 
-void solve(int cnt){
-    if(cnt == 81){
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < N; j++){
-                std::cout << map[i][j] << " ";
+std::pair<int,int> solve(int node, int dist){
+    auto ans = std::make_pair(node, dist);
+    if(visited[node] == true){
+        return std::make_pair(1, 0);
+    }
+
+    visited[node] = true;
+
+    for(auto it = G[node].begin(); it != G[node].end(); it++){
+        int nNode = (*it).first;
+        int nDist = (*it).second;
+//        if(visited[nNode] == false){
+            auto tmp = solve(nNode, dist+nDist);
+            if(tmp.second > ans.second){
+                ans.second = tmp.second;
+                ans.first = tmp.first;
             }
-            std::cout << "\n";
-        }
-        exit(0);
+//        }
     }
 
-    int y = cnt/N;
-    int x = cnt%N;
-
-    if(map[y][x]){
-        solve(cnt+1);
-    }
-    else{
-        for(int i = 1; i <= N; i++){
-            if(col[x][i] == false && row[y][i] == false && box3x3[(y/3)*3 + x/3][i] == false){
-                map[y][x] = i;
-                col[x][i] = row[y][i] = box3x3[(y/3)*3 + x/3][i] = true;
-                solve(cnt+1);
-                map[y][x] = 0;
-                col[x][i] = row[y][i] = box3x3[(y/3)*3 + x/3][i] = false;
-            }
-        }
-    }
+    return ans;
 }
 
 int main(){
     std::cin.tie(nullptr); std::cout.tie(nullptr); std::ios_base::sync_with_stdio(false);
-    map.assign(N, std::vector<int>(N, 0));
-    col.assign(N+1, std::vector<bool>(N+1, false));
-    row.assign(N+1, std::vector<bool>(N+1, false));
-    box3x3.assign(N, std::vector<bool>(N+1, false));
+    std::cin >> n;
 
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
-            std::cin >> map[i][j];
-            if(map[i][j]){
-                row[i][map[i][j]] = true;
-                col[j][map[i][j]] = true;
-                box3x3[(i/3)*3 + j/3][map[i][j]] = true;
-            }
-        }
+    G = std::vector<std::vector<std::pair<int,int>>>(n+1);
+    visited.assign(n+1, false);
+
+    for(int i = 0; i < n-1; i++){
+        int f, t, d;
+        std::cin >> f >> t >> d;
+        G[f].push_back(std::make_pair(t, d));
+        G[t].push_back(std::make_pair(f, d));
+
     }
 
-    solve(0);
+    auto ans = solve(1, 0);
+    std::fill(visited.begin(), visited.end(), false);
+    ans = solve(ans.first, 0);
+
+    std::cout << ans.second << "\n";
     return 0;
 }

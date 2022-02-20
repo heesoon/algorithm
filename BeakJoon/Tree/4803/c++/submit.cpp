038e85b79214bd244fcec79b6b5390a775ed79f9
unknown <chipmaker0304@gmail.com>
2022-02-20 20:30:08 +0900

    complete code

--- BeakJoon/Tree/4803/c++/submit.cpp
+++ BeakJoon/Tree/4803/c++/submit.cpp
@@ -8,12 +8,18 @@ std::vector<std::vector<int>> G;
 std::vector<bool> visited;
 
 bool solve(int idx, int pIdx){
-
+    visited[idx] = true;
+    for(auto nIdx : G[idx]){
+        if(nIdx == pIdx) continue;
+        if(visited[nIdx] == true) return false;
+        if(solve(nIdx, idx) == false) return false;
+    }
+    return true;
 }
 
 int main(){
     std::cin.tie(nullptr); std::cout.tie(nullptr); std::ios_base::sync_with_stdio(false);
-    int tc = 1;
+    int tc = 0;
     while(1){
         int cnt = 0;
         int n, m;
@@ -25,6 +31,7 @@ int main(){
 
         for(int i = 0; i < m; i++){
             int f, t;
+            std::cin >> f >> t;
             G[f].push_back(t);
             G[t].push_back(f);
         }
@@ -35,14 +42,16 @@ int main(){
             }
         }
 
-        std::cout << "Case " << tc << ": ";
-        if(cnt == 0) std::cout << "No trees" << "\n";
-        else if(cnt == 1) std::cout << "There is one tree" << "\n";
-        else std::cout << "A forest of " << cnt << " trees" << "\n";
+        std::cout << "Case " << ++tc;
+        if (cnt == 0) std::cout << ": No trees.";
+        else if (cnt == 1) std::cout << ": There is one tree.";
+        else std::cout << ": A forest of " << cnt << " trees.";
+        std::cout << '\n';
 
         G.clear();
-        std::fill(visited.begin(), visited.end(), false);
-        tc++;
+        visited.clear();
     }
     return 0;
 }
+
+// https://mapocodingpark.blogspot.com/2020/05/4803.html

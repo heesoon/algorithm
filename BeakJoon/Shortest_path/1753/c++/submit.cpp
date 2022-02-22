793a685d2c24b48459c3d2f8fb0a3706f6c2fcd6
heesoon <chipmaker0304@gmail.com>
2022-02-22 13:35:32 +0900

    update

--- BeakJoon/Shortest_path/1753/c++/submit.cpp
+++ BeakJoon/Shortest_path/1753/c++/submit.cpp
@@ -11,8 +11,44 @@ std::vector<int> distance;
 std::vector<bool> visited;
 std::vector<int> S;
 
-void dijkstra(int start){
+int getMinDistIndex(){
+    int minDist = INF;
+    int minIndex = 1;
+    for(int i = 1; i <= V; i++){
+        if(visited[i] == false && minDist > distance[i]){
+            minDist = distance[i];
+            minIndex = i;
+        }
+    }
+    //std::cout << "minIndex : " << minIndex << std::endl;
+    return minIndex;
+}
 
+void dijkstra(int start){
+    distance[start] = 0;
+    visited[start] = true;
+    S.push_back(start);
+    
+    for(int i = 1; i <= V; i++){
+        distance[i] = map[start][i];
+    }
+
+    for(int i = 0; i < V-1; i++){
+        int minIdx = getMinDistIndex();
+        visited[minIdx] = true;
+        S.push_back(minIdx);        
+        for(int j = 1; j <= V; j++){
+            if(visited[j] == false){
+                distance[j] = std::min(distance[j], distance[minIdx] + map[minIdx][j]);
+            }
+        }
+    }
+}
+
+void printDist(){
+    for(int i = 1; i <= V; i++){
+        std::cout << distance[i] << " ";
+    }
 }
 
 int main(){
@@ -23,11 +59,7 @@ int main(){
 
     map.assign(V+1, std::vector<int>(V+1, INF));
     distance.assign(V+1, INF);
-    visited.assign(V+1, INF);
-
-    distance[start] = 0;
-    visited[start] = true;
-    S.push_back(start);
+    visited.assign(V+1, false);
 
     for(int i = 0; i <= V; i++){
         map[i][i] = 0;
@@ -38,5 +70,9 @@ int main(){
         std::cin >> from >> to >> weight;
         map[from][to] = map[to][from] = weight;
     }
+
+    dijkstra(start);
+    printDist();
+
     return 0;
 }
---
+++ BeakJoon/Shortest_path/1753/c++/submit1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <limits>
+
+int main(){
+    std::cin.tie(nullptr); std::cout.tie(nullptr); std::ios_base::sync_with_stdio(false);
+    return 0;
+}

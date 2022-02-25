4f0cc95e19c582cc4c0f526b231ab2d3a06480d4
unknown <chipmaker0304@gmail.com>
2022-02-25 21:01:48 +0900

    complete code

--- BeakJoon/Shortest_path/11657/c++/submit.cpp
+++ BeakJoon/Shortest_path/11657/c++/submit.cpp
@@ -1,62 +1,59 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 #include <algorithm>
 #include <utility>
+#include <limits>
 
-const int N = 9;
-std::vector<std::vector<int>> map;
-std::vector<std::vector<bool>> col;
-std::vector<std::vector<bool>> row;
-std::vector<std::vector<bool>> box3x3;
+int n, m;
+std::vector<std::vector<std::pair<int,int>>> G;
+const int INF = std::numeric_limits<int>::max()/2;
 
-void solve(int cnt){
-    if(cnt == 81){
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < N; j++){
-                std::cout << map[i][j] << " ";
-            }
-            std::cout << "\n";
-        }
-        exit(0);
-    }
+std::vector<long long> solve(int start){
+    bool bcycle = false;
+    std::vector<long long> distance(n+1, INF);
+    distance[start] = 0;
 
-    int y = cnt/N;
-    int x = cnt%N;
+    for(int iter = 0; iter < n; iter++){
+        for(int i = 1; i <= n; i++){
+            for(auto p : G[i]){
+                int nIdx = p.first;
+                int weight = p.second;
 
-    if(map[y][x]){
-        solve(cnt+1);
-    }
-    else{
-        for(int i = 1; i <= N; i++){
-            if(col[x][i] == false && row[y][i] == false && box3x3[(y/3)*3 + x/3][i] == false){
-                map[y][x] = i;
-                col[x][i] = row[y][i] = box3x3[(y/3)*3 + x/3][i] = true;
-                solve(cnt+1);
-                map[y][x] = 0;
-                col[x][i] = row[y][i] = box3x3[(y/3)*3 + x/3][i] = false;
+                if(distance[i] != INF && distance[nIdx] > distance[i] + weight){
+                    distance[nIdx] = distance[i] + weight;
+                    if(iter == n-1){
+                        bcycle = true;
+                    }
+                }
             }
         }
     }
+
+    if(bcycle == true) distance.clear();
+    return distance;
 }
 
 int main(){
     std::cin.tie(nullptr); std::cout.tie(nullptr); std::ios_base::sync_with_stdio(false);
-    map.assign(N, std::vector<int>(N, 0));
-    col.assign(N+1, std::vector<bool>(N+1, false));
-    row.assign(N+1, std::vector<bool>(N+1, false));
-    box3x3.assign(N, std::vector<bool>(N+1, false));
+    std::cin >> n >> m;
 
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
-            std::cin >> map[i][j];
-            if(map[i][j]){
-                row[i][map[i][j]] = true;
-                col[j][map[i][j]] = true;
-                box3x3[(i/3)*3 + j/3][map[i][j]] = true;
-            }
-        }
+    G = std::vector<std::vector<std::pair<int,int>>>(n+1);
+    for(int i = 0; i < m; i++){
+        int a, b, c;
+        std::cin >> a >> b >> c;
+        G[a].push_back(std::make_pair(b, c));
+        //G[b].push_back(std::make_pair(a, c));
     }
 
-    solve(0);
+    auto ans = solve(1);
+
+    if(ans.size() == 0) std::cout << -1 << "\n";
+    else{
+        for(int i = 2; i <= n; i++){
+            if(ans[i] >= INF) std::cout << "-1" << "\n";
+            else std::cout << ans[i] << "\n";
+        }
+    }
     return 0;
 }

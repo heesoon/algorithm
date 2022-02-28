4c722b715e8158858458960a56e72c2f797eec24
heesoon <chipmaker0304@gmail.com>
2022-02-28 10:05:08 +0900

    complete code

--- BeakJoon/Shortest_path/11404/c++/submit.cpp
+++ BeakJoon/Shortest_path/11404/c++/submit.cpp
@@ -2,38 +2,28 @@
 #include <vector>
 #include <algorithm>
 #include <utility>
+#include <limits>
 
-const int N = 9;
-std::vector<std::vector<int>> map;
-std::vector<std::vector<bool>> col;
-std::vector<std::vector<bool>> row;
-std::vector<std::vector<bool>> box3x3;
+int n, m;
+const int INF = std::numeric_limits<int>::max()/2;
+std::vector<std::vector<long long>> G;
 
-void solve(int cnt){
-    if(cnt == 81){
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < N; j++){
-                std::cout << map[i][j] << " ";
-            }
-            std::cout << "\n";
+void printAns(){
+    for(int y = 1; y <= n; y++){
+        for(int x = 1; x <= n; x++){
+            if(G[y][x] == INF) std::cout << 0 << " ";
+            else std::cout << G[y][x] << " ";
         }
-        exit(0);
+        std::cout << "\n";
     }
+    std::cout << "\n";
+}
 
-    int y = cnt/N;
-    int x = cnt%N;
-
-    if(map[y][x]){
-        solve(cnt+1);
-    }
-    else{
-        for(int i = 1; i <= N; i++){
-            if(col[x][i] == false && row[y][i] == false && box3x3[(y/3)*3 + x/3][i] == false){
-                map[y][x] = i;
-                col[x][i] = row[y][i] = box3x3[(y/3)*3 + x/3][i] = true;
-                solve(cnt+1);
-                map[y][x] = 0;
-                col[x][i] = row[y][i] = box3x3[(y/3)*3 + x/3][i] = false;
+void solve(){
+    for(int k = 1; k <= n; k++){
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                    G[i][j] = std::min(G[i][j], G[i][k]+G[k][j]);
             }
         }
     }
@@ -41,22 +31,22 @@ void solve(int cnt){
 
 int main(){
     std::cin.tie(nullptr); std::cout.tie(nullptr); std::ios_base::sync_with_stdio(false);
-    map.assign(N, std::vector<int>(N, 0));
-    col.assign(N+1, std::vector<bool>(N+1, false));
-    row.assign(N+1, std::vector<bool>(N+1, false));
-    box3x3.assign(N, std::vector<bool>(N+1, false));
-
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
-            std::cin >> map[i][j];
-            if(map[i][j]){
-                row[i][map[i][j]] = true;
-                col[j][map[i][j]] = true;
-                box3x3[(i/3)*3 + j/3][map[i][j]] = true;
-            }
-        }
+    std::cin >> n;
+    std::cin >> m;
+    G.assign(n+1, std::vector<long long>(n+1, INF));
+    
+    for(int i = 0; i <= n; i++){
+        G[i][i] = 0;
     }
 
-    solve(0);
+    for(int i = 0; i < m; i++){
+        int from, to, weight;
+        std::cin >> from >> to >> weight;
+        if(G[from][to] > weight) G[from][to] = weight; // 이 부분이 잘 이해가 안됨
+    }
+
+    solve();
+    printAns();
+
     return 0;
 }

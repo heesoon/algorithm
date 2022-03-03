1965063621041aabc36bd29ee7a3313e7d7a72e4
unknown <chipmaker0304@gmail.com>
2022-03-03 13:16:24 +0900

    complete code

--- BeakJoon/Two_point/3273/c++/submit.cpp
+++ BeakJoon/Two_point/3273/c++/submit.cpp
@@ -4,8 +4,44 @@
 #include <utility>
 #include <limits>
 
+std::vector<int> arr;
+
+int solve(int n, int x){
+    int ans = 0;
+    std::sort(arr.begin(), arr.end());
+    int lo = 0, hi = n-1;
+
+    while(lo < hi){
+        if(arr[lo] + arr[hi] == x){
+            ans++;
+            lo++;
+            hi--;
+        }
+        else if(arr[lo] + arr[hi] > x){
+            hi--;
+        }
+        else{
+            lo++;
+        }
+    }
+
+    return ans;
+}
+
 int main(){
     std::cin.tie(nullptr); std::cout.tie(nullptr); std::ios_base::sync_with_stdio(false);
+    int n, x;
+    std::cin >> n;
+
+    for(int i = 0; i < n; i++){
+        int x;
+        std::cin >> x;
+        arr.push_back(x);
+    }
+
+    std::cin >> x;
+
+    std::cout << solve(n, x) << "\n";
 
     return 0;
 }

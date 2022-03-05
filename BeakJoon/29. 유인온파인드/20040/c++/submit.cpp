52283a6b109bac9a45b7137da8783affc0bf65b2
unknown <chipmaker0304@gmail.com>
2022-03-05 20:43:52 +0900

    complete code

--- BeakJoon/29. 유인온파인드/20040/c++/submit.cpp
+++ BeakJoon/29. 유인온파인드/20040/c++/submit.cpp
@@ -4,8 +4,46 @@
 #include <utility>
 #include <limits>
 
+std::vector<int> vParents;
+
+int doFind(int x){
+    if(vParents[x] == x) return x;
+    else return vParents[x] = doFind(vParents[x]); 
+}
+
+void doUnion(int a, int b){
+    int pa = doFind(a);
+    int pb = doFind(b);
+
+    if(pa < pb){
+        vParents[pb] = pa;
+    }
+    else{
+        vParents[pa] = pb;
+    }
+}
+
 int main(){
     std::cin.tie(nullptr); std::cout.tie(nullptr); std::ios_base::sync_with_stdio(false);
+    int n, m, ans = 0;
+    std::cin >> n >> m;
 
+    vParents.assign(n, 0);
+    for(int i = 0; i < n; i++){
+        vParents[i] = i;
+    }
+
+    for(int i = 1; i <= m; i++){
+        int a, b;
+        std::cin >> a >> b;
+        if(doFind(a) == doFind(b)){
+            ans = i;
+            break;
+        }
+
+        doUnion(a, b);
+    }
+
+    std::cout << ans << "\n";
     return 0;
 }

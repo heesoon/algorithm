929dc372f8e1b2de2ffa6225051915379fee7204
unknown <chipmaker0304@gmail.com>
2022-03-06 00:17:58 +0900

    complete code

--- BeakJoon/30. 최소신장트리/9372/c++/submit.cpp
+++ BeakJoon/30. 최소신장트리/9372/c++/submit.cpp
@@ -6,6 +6,20 @@
 
 int main(){
     std::cin.tie(nullptr); std::cout.tie(nullptr); std::ios_base::sync_with_stdio(false);
+    int T;
+    std::cin >> T;
+
+    while(T--){
+        int N, M;
+        std::cin >> N >> M;
+
+        for(int i = 0; i < M; i++){
+            int a, b;
+            std::cin >> a >> b;
+        }
+
+        std::cout << N-1 << "\n";
+    }
 
     return 0;
 }

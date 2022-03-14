32b8645f91e8612f5224a029b9c04e440e5f52e2
heesoon <chipmaker0304@gmail.com>
2022-03-14 13:51:29 +0900

    complete code

--- BeakJoon/36. 최소공통조상/3176/c++/submit.cpp
+++ BeakJoon/36. 최소공통조상/3176/c++/submit.cpp
@@ -1,84 +1,101 @@
 #include <iostream>
+#include <utility>
+#include <algorithm>
 #include <vector>
 #include <queue>
-#include <algorithm>
-#include <utility>
 #include <limits>
 #include <cmath>
 
-const int H = std::ceil(std::log2(100'000));
+int N, M;
+const int NMAX = 1e5;
+const int KMAX  = std::ceil(std::log2(100'000));
 
-std::vector<bool> vVisited;
-std::vector<int> vDepth;
+std::vector<int> vDepth(NMAX+1, 0);
+std::vector<std::vector<std::pair<int,int>>> vDist;
 std::vector<std::vector<std::pair<int,int>>> vMap;
-std::vector<std::vector<int>> vSparseTables;
-std::vector<std::vector<int>> vMinDists;
-std::vector<std::vector<int>> vMaxDists;
+std::vector<std::vector<int>> vSparseTable(NMAX+1, std::vector<int>(KMAX+1, 0));
 
-std::pair<int,int> solve(int u, int v){
-    int retMin = std::numeric_limits<int>::max()/2;
-    int retMax = 0;
+void buildTreeByBFS(int start){
+    std::queue<int> Q;
+    Q.push(start);
+    vDepth[start] = 1;
 
-    std::cout << "u, v : " << u << " , " << v << std::endl;
-    if(vDepth[u] != vDepth[v]){
-        if(vDepth[u] > vDepth[v])
-            std::swap(u, v);
-        
-        for(int i = H; i >= 0; i--){
-            if(vDepth[u] <= vDepth[vSparseTables[v][i]]){
-                std::cout << i << " : " << u << ", " << v << std::endl;
-                retMin = std::min(retMin, vMinDists[v][i]);
-                retMax = std::max(retMax, vMaxDists[v][i]);
-                std::cout << i << " : " << retMin << ", " << retMax << std::endl;
-                v = vSparseTables[v][i];
-            }
+    while(!Q.empty()){
+        auto cIdx = Q.front();
+        Q.pop();
+
+        for(const auto &p : vMap[cIdx]){
+            auto nIdx = p.first;
+            auto dist = p.second;
+
+            if(vDepth[nIdx] != 0) continue;
+            vDepth[nIdx] = vDepth[cIdx]+1;
+            vSparseTable[nIdx][0] = cIdx;
+            vDist[nIdx][0].first = vDist[nIdx][0].second = dist;
+            Q.push(nIdx);
         }
     }
+}
 
-    int lcu = u, lcv = v;
+void buildTreeByDFS(int cIdx){
+    for(auto p : vMap[cIdx]){
+        int nIdx = p.first;
+        int dist = p.second;
 
-    if(u != v){
-        for(int i = H; i >= 0; i--){
-            //std::cout << "out --  " << vSparseTables[u][i] << ", " << vSparseTables[v][i] << std::endl;
-            if(vSparseTables[u][i] != 0 && vSparseTables[u][i] != vSparseTables[v][i]){
-                u = vSparseTables[u][i];
-                v = vSparseTables[v][i];
+        if(vDepth[nIdx] != 0) continue;
 
-                //std::cout << i << " : " << u << ", " << v << std::endl;
+        vDepth[nIdx] = vDepth[cIdx]+1;
+        vSparseTable[nIdx][0] = cIdx;
+        vDist[nIdx][0].first = vDist[nIdx][0].second = dist;
+        buildTreeByDFS(nIdx);
+    }
+}
 
-                retMin = std::min(retMin, vMinDists[u][i]);
-                retMin = std::min(retMin, vMinDists[v][i]);
-                retMax = std::max(retMax, vMaxDists[u][i]);
-                retMax = std::max(retMax, vMaxDists[v][i]);
+std::pair<int,int> solve(int a, int b){
+    int minValue = std::numeric_limits<int>::max()/2;
+    int maxVaule = 0;
+    int kIdx = 0;
 
-                //std::cout << i << " : " << retMin << ", " << retMax << std::endl;
-            }
-            lcu = vSparseTables[u][i];
-            lcv = vSparseTables[v][i];
-            //std::cout << "lcu = " << lcu << ", " << "lcv = " << lcv << std::endl;
-        }
-
-                retMin = std::min(retMin, vMinDists[u][0]);
-                retMin = std::min(retMin, vMinDists[v][0]);
-                retMax = std::max(retMax, vMaxDists[u][0]);
-                retMax = std::max(retMax, vMaxDists[v][0]);        
+    if(vDepth[a] < vDepth[b]){
+        std::swap(a, b);
     }
 
-    return std::make_pair(retMin, retMax);
+    int diff = vDepth[a] - vDepth[b];
+
+    while(diff){
+        if((diff&0x01) == 1){
+            minValue = std::min(minValue, vDist[a][kIdx].first);
+            maxVaule = std::max(maxVaule, vDist[a][kIdx].second);
+            a = vSparseTable[a][kIdx];
+        }
+
+        diff >>= 1;
+        kIdx++;
+    }
+
+    if(a != b){
+        for(int k = KMAX; k >= 0; k--){
+            if(vSparseTable[a][k] != vSparseTable[b][k]){
+                minValue = std::min(minValue, std::min(vDist[a][k].first, vDist[b][k].first));
+                maxVaule = std::max(maxVaule, std::max(vDist[a][k].second, vDist[b][k].second));
+                a = vSparseTable[a][k];
+                b = vSparseTable[b][k];
+            }
+        }
+
+        minValue = std::min(minValue, std::min(vDist[a][0].first, vDist[b][0].first));
+        maxVaule = std::max(maxVaule, std::max(vDist[a][0].second, vDist[b][0].second));          
+    }
+
+    return std::make_pair(minValue, maxVaule);
 }
 
 int main(){
     std::cin.tie(nullptr); std::cout.tie(nullptr); std::ios_base::sync_with_stdio(false);
-    int N;
     std::cin >> N;
 
-    vDepth.assign(N+1, 0); vDepth[0] = -1;
-    vVisited.assign(N+1, false); vVisited[0] = true;
     vMap = std::vector<std::vector<std::pair<int,int>>>(N+1);
-    vSparseTables.assign(N+1, std::vector<int>(H+1, 0));
-    vMinDists.assign(N+1, std::vector<int>(H+1, std::numeric_limits<int>::max()/2));
-    vMaxDists.assign(N+1, std::vector<int>(H+1, 0));
-
+    vDist.assign(NMAX+1, std::vector<std::pair<int,int>>(KMAX+1, std::make_pair(0, 0)));
     for(int i = 0; i < N-1; i++){
         int a, b, c;
         std::cin >> a >> b >> c;
@@ -86,48 +103,30 @@ int main(){
         vMap[b].push_back(std::make_pair(a, c));
     }
 
-    // build sparse table
-    std::queue<int> Q;
-    Q.push(1); vVisited[1] = true;
+    vDepth[1] = 1;
 
-    while(Q.empty() == false){
-        auto fVertex = Q.front();
-        Q.pop();
-        
-        for(const auto &p : vMap[fVertex]){
-            auto nVertex = p.first;
-            auto dist = p.second;
+    //buildTreeByDFS(1);
+    buildTreeByBFS(1);
 
-            if(vVisited[nVertex] == false){
-                vVisited[nVertex] = true;
-                vDepth[nVertex] = vDepth[fVertex]+1;
-                vSparseTables[nVertex][0] = fVertex; // 2^0
-                vMinDists[nVertex][0] = vMaxDists[nVertex][0] = dist;
-
-                for(int i = 1; i <= H; i++){
-                    auto prevParentIdx = vSparseTables[nVertex][i-1];
-                    vSparseTables[nVertex][i] = vSparseTables[prevParentIdx][i-1];
-                    vMinDists[nVertex][i] = std::min(vMinDists[nVertex][i-1], vMinDists[prevParentIdx][i-1]);
-                    vMaxDists[nVertex][i] = std::min(vMaxDists[nVertex][i-1], vMaxDists[prevParentIdx][i-1]);
-                    if(vSparseTables[nVertex][i] == 0){
-                        break;
-                    }
-                }
-
-                Q.push(nVertex);
+    // build dists and parents of K
+    for(int k = 0; k <= KMAX; ++k){
+        for(int i = 2; i <= N; ++i){
+            int father = vSparseTable[i][k];
+            if(father){
+                vSparseTable[i][k+1] = vSparseTable[father][k];
+                vDist[i][k+1].first = std::min(vDist[i][k].first, vDist[father][k].first);
+                vDist[i][k+1].second = std::max(vDist[i][k].second, vDist[father][k].second);
             }
         }
     }
 
-    int M;
     std::cin >> M;
+
     for(int i = 0; i < M; i++){
-        int u, v;
-        std::cin >> u >> v;
-        auto ans = solve(u, v);
+        int a, b;
+        std::cin >> a >> b;
+        auto ans = solve(a, b);
         std::cout << ans.first << " " << ans.second << "\n";
     }
     return 0;
 }
-
-// https://everenew.tistory.com/96

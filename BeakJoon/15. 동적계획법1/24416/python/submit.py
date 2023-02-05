5ca89f621063e6f6d35d0aed3356d9d757e6d7e8
unknown <chipmaker0304@gmail.com>
2023-02-06 00:45:38 +0900

    fix problems

--- BeakJoon/15. 동적계획법1/24416/python/submit.py
+++ BeakJoon/15. 동적계획법1/24416/python/submit.py
@@ -1,6 +1,6 @@
 
-mem = [-1]*40
-mem[0], mem[1], mem[2] = 0, 1, 1
+dp = [-1]*(41)
+dp[0], dp[1], dp[2] = 0, 1, 1
 
 def fibonacci(n):
     if n == 1 or n == 2:
@@ -13,13 +13,13 @@ def fibonacci2(n):
     if n <= 0:
         return 0, 0
     
-    if mem[n] != -1:
-        return mem[n] , 0
+    if dp[n] != -1:
+        return dp[n] , 0
 
     a, b = fibonacci2(n-1)
     c, d = fibonacci2(n-2)
-    mem[n], cnt = a + c, b + d + 1
-    return mem[n], cnt
+    dp[n], cnt = a + c, b + d + 1
+    return dp[n], cnt
 
 n = int(input())
 
--- BeakJoon/15. 동적계획법1/24416/python/submit2.py
+++ BeakJoon/15. 동적계획법1/24416/python/submit2.py
@@ -1,7 +1,4 @@
 
-mem = [-1]*40
-mem[0], mem[1], mem[2] = 0, 1, 1
-
 def fibonacci(n):
     if n == 1 or n == 2:
         return 1
@@ -9,18 +6,16 @@ def fibonacci(n):
     return fibonacci(n-1) + fibonacci(n-2)
 
 def fibonacci2(n):
-    #print("fibonacci2", n)
-    if n <= 0:
-        return 0, 0
+    cnt = 0
+    dp = [-1]*(n+1)
+    dp[0], dp[1], dp[2] = 0, 1, 1
     
-    if mem[n] != -1:
-        return mem[n] , 0
+    for i in range(3, n+1):
+        cnt += 1
+        dp[i] = dp[i-1] + dp[i-2]
 
-    a, b = fibonacci2(n-1)
-    c, d = fibonacci2(n-2)
-    mem[n], cnt = a + c, b + d + 1
-    return mem[n], cnt
+    return cnt
 
 n = int(input())
 
-print(fibonacci(n), fibonacci2(n)[1])
+print(fibonacci(n), fibonacci2(n))
